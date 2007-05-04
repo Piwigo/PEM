29f1ae7ca74a3c82c15b49e1d4875c4d569c55d7
plegall <plg@piwigo.org>
2007-05-04 09:29:43 +0000

    New: script to generate the database creation file
    git-svn-id: http://piwigo.org/pem_svn/trunk@22 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ install/generate_dbcreate_file.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# usage: ./generate_dbcreate_file.sh root conway pwg_forum pwg_ > pem.sql
+
+user=$1
+pass=$2
+database=$3
+prefix=$4
+
+echo "show tables like '$prefix%'" > /tmp/generate_dbcreate_file.$$
+
+mysqldump \
+  --no-defaults \
+  --user=$user \
+  --password=$pass \
+  --add-drop-table \
+  --no-data \
+  $database \
+  $(mysql \
+    --user=$user \
+    --password=$pass \
+    $database \
+    < /tmp/generate_dbcreate_file.$$ \
+    | grep ^$prefix \
+    | perl -ne 'END {print join(" ", @tables)} chomp; push @tables, $_;' \
+  ) \
+  | perl -pe "s{$prefix}{pem_}g; s{ ENGINE=MyISAM DEFAULT CHARSET=latin1}{}g;" \
+  | grep -v '^/\*' \
+  | grep -v '^--' \
+  | grep -v ^$
