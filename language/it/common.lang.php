ea4fad17b4b96c8d7fd14a8bc0238e71df5c7a8c
ddtddt <ddtddt@piwigo.org>
2009-01-18 23:26:00 +0000

    Translation Italian - Thanks to RIO
    git-svn-id: http://piwigo.org/pem_svn/trunk@109 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/it/common.lang.php
@@ -0,0 +1,134 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | last modifier : $Author$
+// | revision      : $Revision$
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'Descrzione';
+$lang['Add/Modify an extension'] = 'Aggiungere/modificare un\'estensione';
+$lang['Add a link'] = 'Aggiungere un link';
+$lang['Add a revision'] = 'Aggiungere una revisione';
+$lang['Add or replace screenshot'] = 'Aggiungere o rimpiazzare un\'immagine';
+$lang['Add an extension'] = 'Aggiungere un\'estensione';
+$lang['Administration'] = 'Amministrazione';
+$lang['Agreement'] = 'Accordo';
+$lang['agreement_description'] = 'Accetto i termini del contratto di contribuzione che stipula il trasferimento della mia proprietà intellettuale.';
+$lang['Are you sure you want to delete this item?'] = 'Siete sicuro di volere cancellare quest\'elemento?';
+$lang['Author'] = 'Autore';
+$lang['Automatic redirection in %s seconds.'] = 'Ridirezzione automatica tra %s secondi.';
+$lang['Back to previous page'] = 'Ritornare alla pagina precedente';
+$lang['Can only resize PNG and JPEG files'] = 'Ridimensionamento possibile unicamente per i files PNG e JPEG';
+$lang['Categories'] = 'Categorie';
+$lang['Category'] = 'Categoria';
+$lang['Changes'] = 'Cambiamenti';
+$lang['Click here if don\'t want to wait.'] = 'Cliccate qui se non volete aspettare.';
+$lang['Confirmation password does not match.'] = 'La password di conferma non corrisponde.';
+$lang['Confirm password'] = 'Confermare la password';
+$lang['compact view'] = 'visualizzazione compatta';
+$lang['Compatibility'] = 'Compatibilità';
+$lang['Compatible with'] = 'Compatibile con';
+$lang['Connection settings'] = 'Connessione';
+$lang['Deletion forbidden'] = 'Cancellazione impossibile';
+$lang['Delete extension'] = 'Cancellare l\'estensione';
+$lang['Delete link'] = 'Cancellare il link';
+$lang['Delete revision'] = 'Cancellare la revisione';
+$lang['Delete screenshot'] = 'Cancellare l\'immagine';
+$lang['Description'] = 'Descrizione';
+$lang['Disclaimer'] = 'Disclaimer';
+$lang['Disconnect'] = 'Esci';
+$lang['Download revision'] = 'Scaricare la revisione';
+$lang['Downloads'] = 'Download';
+$lang['expand/collapse all'] = 'Espandere/Ridurre tutto';
+$lang['extensions'] = 'estensioni';
+$lang['Extension informations'] = 'Informazioni del\'estensione';
+$lang['Extension successfuly added. Thank you.'] = 'L\'estensione è stata aggiunta con successo. Grazie.';
+$lang['Extension successfuly deleted.'] = 'L\'estensione è stata cancellata con successo.';
+$lang['Error'] = 'Errore';
+$lang['File'] = 'File';
+$lang['File too big. Filesize must not exceed %s.'] = 'File troppo voluminoso. La tailia non deve eccedere %s.';
+$lang['Filter'] = 'Filtrare';
+$lang['first'] = 'Prima';
+$lang['First revision date'] = 'Data della prima revisione';
+$lang['Hello %s'] = 'Salve %s';
+$lang['Home'] = 'Home';
+$lang['Identification successful'] = 'Identificazione eseguita';
+$lang['Incorrect extension identifier'] = 'Id d\'estensione scorretta';
+$lang['Incorrect revision identifier'] = 'Id di revisione scorretta';
+$lang['Incorrect URL'] = 'URL scorretta';
+$lang['Incorrect username'] = 'Nome d\'utente scorretto';
+$lang['Incorrect username/password'] = 'Nome d\'utente o password scorrectto';
+$lang['Index'] = 'Indice';
+$lang['last'] = 'Ultima';
+$lang['Latest revision date'] = 'Data dell\'ultima revisione';
+$lang['Link name must not be empty'] = 'Il nome del link non può essere vuoto';
+$lang['Login'] = 'Identificazione';
+$lang['Logout successful'] = 'Logout effettuato';
+$lang['Mail address'] = 'Indirizzo mail';
+$lang['Manage links'] = 'Generare i link';
+$lang['Manage screenshots'] = 'Generare le immagini';
+$lang['Modify extension'] = 'Modificare l\'estensione';
+$lang['Modify revision'] = 'Modificare la revisione';
+$lang['Most recent extensions'] = 'Ultime estensioni';
+$lang['My extensions'] = 'Le mie estensioni';
+$lang['My personnal homepage'] = 'Pagina personale';
+$lang['Name'] = 'Nome';
+$lang['news feed'] = 'Filo RSS';
+$lang['next'] = 'Successiva';
+$lang['No account?'] = 'Nessun\' conto?';
+$lang['No extension match your filter'] = 'Nessun\' estensione corrisponde al vostro filtro';
+$lang['No revision available for this extension.'] = 'Nessuna revisione disponibile per questa estensione. E possibile che non esistono revisioni per questa estensione, o nessuna revisione corrisponde al vostro filtro.';
+$lang['no revision yet'] = 'nessuna revisione';
+$lang['Notes'] = 'Note';
+$lang['Only *.zip files are allowed'] = 'Solo i file *.zip sono autorizzati';
+$lang['page generated in %s'] = 'pagina generata in %s';
+$lang['Password'] = 'Password';
+$lang['Personnal informations'] = 'Informazioni personali';
+$lang['powered by'] = 'realizzato grazie a <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'Precedente';
+$lang['Problem during upload'] = 'Un problema è avvenuto durante l\'upload';
+$lang['Rank'] = 'Rango';
+$lang['Register'] = 'Registrarsi';
+$lang['Related links'] = 'Link';
+$lang['Released on'] = 'Aggiunto il';
+$lang['Reset'] = 'Reinizializzare';
+$lang['Revision'] = 'Revisione';
+$lang['Revision list'] = 'Lista delle revisioni';
+$lang['Revision successfuly added. Thank you.'] = 'La revisione è stata aggiunta con successo. Grazie.';
+$lang['Revision successfuly deleted.'] = 'La revisione è stata cancellata con successo.';
+$lang['Search'] = 'Cercare';
+$lang['See extension'] = 'Vedere l\'estensione';
+$lang['Some fields are missing'] = 'Mancano dei campi';
+$lang['standard view'] = 'visualizzazione standard';
+$lang['Submit'] = 'Confermare';
+$lang['Success'] = 'Operazione riuscita';
+$lang['undefined extension identifier'] = 'Id d\'estensione sconosciuta';
+$lang['undefined revision identifier'] = 'Id della revisione sconosciuta';
+$lang['Username already in use'] = 'Il nome d\'utente è già stato preso';
+$lang['Unknown extension'] = 'Estensione sconosciuta';
+$lang['URL'] = 'URL';
+$lang['Username'] = 'Nome utente';
+$lang['Version'] = 'Versione';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'Potete solo caricare dei file PNG ou JPEG.';
+$lang['You did not upload anything!'] = 'Non potete niente caricare!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'Dovete essere connesso per aggiungere, modificare o cancellare un\'estensione.';
+$lang['You must be connected to reach this page.'] = 'Dovete essere connesso per accedere a questa paggina.';
+$lang['You must be the extension author to modify it.'] = 'Dovete essere l\'autore del\'estensione per modificarla.';
+
+?>
