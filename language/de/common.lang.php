185c33f0c0b60ac32611c8c265397a2bf5f643e3
ddtddt <ddtddt@piwigo.org>
2009-01-19 16:19:08 +0000

    Translation German - Thanks to Daniel
    git-svn-id: http://piwigo.org/pem_svn/trunk@113 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/de/common.lang.php
@@ -0,0 +1,134 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | last modifier : $Author$
+// | revision      : $Revision$
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = '&Uuml;ber';
+$lang['Add/Modify an extension'] = 'Hinzuf&uuml;gen/&Auml;ndern einer Erweiterung';
+$lang['Add a link'] = 'F&uuml;ge einen Link hinzu';
+$lang['Add a revision'] = 'F&uuml;ge eine Revision hinzu';
+$lang['Add an extension'] = 'F&uuml;ge eine Erweiterung hinzu';
+$lang['Add or replace screenshot'] = '&Auml;ndere oder f&uuml;ge einen Screenshot hinzu';
+$lang['Administration'] = 'Administration';
+$lang['Agreement'] = 'Vereinbarung';
+$lang['agreement_description'] = 'Ich nehme diese Veinbarung an, die mein geistiges Eigentum auf meinem Beitrag &uuml;bernimmt.';
+$lang['Are you sure you want to delete this item?'] = 'Bist du sicher, das du es wirklich l&ouml;schen willst?';
+$lang['Author'] = 'Autor';
+$lang['Automatic redirection in %s seconds.'] = 'Sie werden in %s Sekunden weitergeleitet.';
+$lang['Back to previous page'] = 'Zur&uuml;ck zur vorhergehenden Seite';
+$lang['Can only resize PNG and JPEG files'] = 'Nur PNG und JPEG Dateien k&ouml;nnen verarbeitet werden';
+$lang['Categories'] = 'Kategorien';
+$lang['Category'] = 'Kategorie';
+$lang['Changes'] = '&Auml;nderungen';
+$lang['Click here if don\'t want to wait.'] = 'Klicke hier wenn du nicht warten m&ouml;chtest.';
+$lang['Confirmation password does not match.'] = 'Die Best&auml;tigung des Passwortes ist falsch..';
+$lang['Confirm password'] = 'Best&auml;tige Passwort';
+$lang['compact view'] = 'Kompaktansicht';
+$lang['Compatibility'] = 'Kompatibilit&auml;t';
+$lang['Compatible with'] = 'Kompatibel mit';
+$lang['Connection settings'] = 'Login Einstellungen';
+$lang['Deletion forbidden'] = 'L&ouml;schen nicht erlaubt';
+$lang['Delete extension'] = 'L&ouml;sche Erweiterung';
+$lang['Delete link'] = 'L&ouml;sche Link';
+$lang['Delete revision'] = 'L&ouml;sche Revision';
+$lang['Delete screenshot'] = 'L&ouml;sche Screenshot';
+$lang['Description'] = 'Beschreibung';
+$lang['Disclaimer'] = 'Ausschlussklausel';
+$lang['Disconnect'] = 'Abmelden';
+$lang['Download revision'] = 'Revision herunterladen';
+$lang['Downloads'] = 'Downloads';
+$lang['Error'] = 'Fehler';
+$lang['expand/collapse all'] = 'Alles aus-/einklappen';
+$lang['extensions'] = 'Erweiterungen';
+$lang['Extension informations'] = 'Erweiterungsinformationen';
+$lang['Extension successfuly added. Thank you.'] = 'Erweiterung erfolgreich hinzugef&uuml;gt. Herzlichen Dank.';
+$lang['Extension successfuly deleted.'] = 'Erweiterung erfolgreich gel&ouml;scht.';
+$lang['File'] = 'Datei';
+$lang['File too big. Filesize must not exceed %s.'] = 'Datei zu gro&szlig;. Die Dateigr&ouml;&szlig;e darf nicht gr&ouml;&szlig;er als %s sein.';
+$lang['Filter'] = 'Filter';
+$lang['first'] = 'Erste';
+$lang['First revision date'] = 'Datum der ersten Revision';
+$lang['Hello %s'] = 'Hallo %s';
+$lang['Home'] = 'Home';
+$lang['Identification successful'] = 'Anmeldung erfolgreich';
+$lang['Incorrect extension identifier'] = 'Fehlerhafte Erweiterungskennzeichnung';
+$lang['Incorrect revision identifier'] = 'Fehlerhafte Revisionsskennzeichnung';
+$lang['Incorrect URL'] = 'Fehlerhafte URL';
+$lang['Incorrect username'] = 'Falscher Benutzername';
+$lang['Incorrect username/password'] = 'Fehlerhafter Benutzername oder Passwort';
+$lang['Index'] = 'Index';
+$lang['last'] = 'Letzte';
+$lang['Latest revision date'] = 'Letztes Revisionsdatum';
+$lang['Link name must not be empty'] = 'Der Linkname darf nicht leer sein';
+$lang['Login'] = 'Anmeldung';
+$lang['Logout successful'] = 'Erfolgreich abgemeldet';
+$lang['Mail address'] = 'E-Mailadresse';
+$lang['Manage links'] = 'Manage Links';
+$lang['Manage screenshots'] = 'Manage Screenshots';
+$lang['Modify extension'] = 'Bearbeite Erweiterung';
+$lang['Modify revision'] = 'Bearbeite Revision';
+$lang['Most recent extensions'] = 'Die beliebtesten Erweiterungen';
+$lang['My extensions'] = 'Meine Erweiterungen';
+$lang['My personnal homepage'] = 'Meine pers&ouml;nliche Homepage';
+$lang['Name'] = 'Name';
+$lang['news feed'] = 'News Feed';
+$lang['next'] = 'N&auml;chste';
+$lang['No account?'] = 'Keinen Account?';
+$lang['No extension match your filter'] = 'Keine Erweiterung entspricht deinem Filter';
+$lang['No revision available for this extension.'] = 'F&uuml;r diese Revision gibt es keine Erweiterung. Entweder weil es &uuml;berhaupt keine Revision gibt oder weil es keine mit dem Versionsfilter kompatible Revision gibt,.';
+$lang['no revision yet'] = 'noch keine Revision';
+$lang['Notes'] = 'Bemerkungen';
+$lang['Only *.zip files are allowed'] = 'Nur *.zip Dateien sind erlaubt';
+$lang['page generated in %s'] = 'Seite erstellt in %s';
+$lang['Password'] = 'Passwort';
+$lang['Personnal informations'] = 'Pers&ouml;nliche Informationen';
+$lang['powered by'] = 'powered by <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'Vorhergehende';
+$lang['Problem during upload'] = 'Problem w&auml;hrend des Uploades';
+$lang['Rank'] = 'Rang';
+$lang['Register'] = 'Registrieren';
+$lang['Related links'] = 'Verwandte Links';
+$lang['Released on'] = 'Freigegeben am';
+$lang['Reset'] = 'Reset';
+$lang['Revision'] = 'Revision';
+$lang['Revision list'] = 'Revisionsliste';
+$lang['Revision successfuly added. Thank you.'] = 'Revision erfolgreich hinzugef&uuml;gt. Herzlichen Dank.';
+$lang['Revision successfuly deleted.'] = 'Revision erfolgreich gel&ouml;scht.';
+$lang['Search'] = 'Suchen';
+$lang['See extension'] = 'Zeige Erweiterungen';
+$lang['Some fields are missing'] = 'Einige Felder fehlen';
+$lang['standard view'] = 'Standartansicht';
+$lang['Submit'] = 'Best&auml;tigen';
+$lang['Success'] = 'Erfolgreich';
+$lang['undefined extension identifier'] = 'undefinierte Erweiterungskennzeichnung';
+$lang['undefined revision identifier'] = 'undefinierte Revisionskennzeichnung';
+$lang['Username already in use'] = 'Der Benutzername wird schon benutzt';
+$lang['Unknown extension'] = 'Unbekannte Erweiterung';
+$lang['URL'] = 'URL';
+$lang['Username'] = 'Benutzername';
+$lang['Version'] = 'Version';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'Du kannst nur PNG und JPEG Dateien als Screenshot benutzen.';
+$lang['You did not upload anything!'] = 'Du hast nichts hochgeladen!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'Du musst angemeldet sein um eine Erweiterung zu bearbeiten oder  l&ouml;schen.';
+$lang['You must be connected to reach this page.'] = 'Du musst angemeldet sein um diese Seite zu sehen.';
+$lang['You must be the extension author to modify it.'] = 'Nur der Autor selbst kann eine Erweiterung weiterbearbeiten.';
+
+?>
