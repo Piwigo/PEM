3b2c5194cd82becf308e3c53651e9b808814f7c5
plegall <plg@piwigo.org>
2009-07-03 14:58:55 +0000

    bug #13853: feature added, ability to fetch the category list from the API.
    git-svn-id: http://piwigo.org/pem_svn/trunk@162 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ api/get_category_list.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | last modifier : $Author: plg $
+// | revision      : $Revision: 2 $
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+define('INTERNAL', true);
+$root_path = '../';
+require_once($root_path.'include/common.inc.php');
+
+$query = '
+SELECT
+    idx_category AS id,
+    name,
+    COUNT(*) AS counter
+  FROM '.EXT_CAT_TABLE.'
+    JOIN '.CAT_TABLE.' ON idx_category = id_category
+  GROUP BY idx_category
+;';
+$output = array_of_arrays_from_query($query);
+foreach ($output as $id => $category) {
+  $output[$id]['name'] = get_user_language($output[$id]['name']);
+}
+
+$format = 'json';
+if (isset($_GET['format'])) {
+  $format = strtolower($_GET['format']);
+}
+
+switch ($format) {
+  case 'json' :
+    echo json_encode($output);
+    break;
+  case 'php' :
+    echo serialize($output);
+    break;
+  default :
+    echo json_encode($output);
+}
+?>
