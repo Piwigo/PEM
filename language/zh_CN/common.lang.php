8c6c083a31a2e4ef95d523ce6dd9d1d4e5af54cd
plegall <plg@piwigo.org>
2009-11-04 21:33:02 +0000

    new: PEM web interface translated into simplified chinese by Poon Yuet Ho (Eliot Poon). I've updated the iso code because traditionnal chinese is arriving and I wanted an easier way to read simplified/traditionnal chinese code.
    git-svn-id: http://piwigo.org/pem_svn/trunk@212 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/zh_CN/common.lang.php
@@ -0,0 +1,171 @@
+﻿
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = '关于我们';
+$lang['Add/Modify an extension'] = '新增/修改扩充组件';
+$lang['Add a link'] = '新增连结';
+$lang['Add a revision'] = '新增修订版本';
+$lang['Add an extension'] = '新增扩充组件';
+$lang['Add or replace screenshot'] = '新增或取代截图';
+$lang['Administration'] = '管理';
+$lang['Agreement'] = '同意书';
+$lang['agreement_description'] = '就有关本人所贡献的知识产权, 本人接纳此同意书内的有关条款。 I accept this terms and conditions agreement that take my intellectual property on my contribution.';
+$lang['Are you sure you want to delete this item?'] = '你是否确认删除此项目?';
+$lang['Author'] = '作者';
+$lang['Automatic redirection in %s seconds.'] = '在 %s 秒后自动转页';
+$lang['Back to previous page'] = '回前页';
+$lang['Can only resize PNG and JPEG files'] = '只能够重新设定PNG及JPEG档案的大小。';
+$lang['Categories'] = '类别';
+$lang['Category'] = '类别';
+$lang['Changes'] = '变更';
+$lang['Click here if don\'t want to wait.'] = '如不想等候,请按此处。';
+$lang['Confirmation password does not match.'] = '确认密码不正确';
+$lang['Confirm password'] = '密码确认';
+$lang['compact view'] = '精简检视';
+$lang['Compatibility'] = '兼容性';
+$lang['Compatible with'] = '相容于';
+$lang['Connection settings'] = '连结设定';
+$lang['Deletion forbidden'] = '已禁止删除';
+$lang['Delete extension'] = '删除扩充组件';
+$lang['Delete link'] = '删除连结';
+$lang['Delete revision'] = '删除修订版本';
+$lang['Delete screenshot'] = '删除截图';
+$lang['Description'] = '描述';
+$lang['Disclaimer'] = '免责条款';
+$lang['Disconnect'] = '中断';
+$lang['Download revision'] = '下载修订版本';
+$lang['Downloads'] = '下载';
+$lang['Error'] = '错误';
+$lang['expand/collapse all'] = '全部展开/收合';
+$lang['extensions'] = '扩充组件';
+$lang['Extension informations'] = '扩充组件信息';
+$lang['Extension successfuly added. Thank you.'] = '成功新增扩充组件, 多谢';
+$lang['Extension successfuly deleted.'] = '成功删除扩充组件';
+$lang['File'] = '档案';
+$lang['File too big. Filesize must not exceed %s.'] = '档案太大。档案大小不能超过%。';
+$lang['Filter'] = '过泸';
+$lang['first'] = '起始';
+$lang['First revision date'] = '起始版本日期';
+$lang['Hello %s'] = '你好 %';
+$lang['Home'] = '首页';
+$lang['Identification successful'] = '识别成功 ';
+$lang['Incorrect extension identifier'] = '扩充组件识别不符';
+$lang['Incorrect revision identifier'] = '版本识别不符';
+$lang['Incorrect URL'] = '网络地址不符';
+$lang['Incorrect username'] = '使用者名称不符';
+$lang['Incorrect username/password'] = '用户名称/密码不符';
+$lang['Index'] = '索引';
+$lang['last'] = '最后';
+$lang['Latest revision date'] = '最后版本日期';
+$lang['Link name must not be empty'] = '连结名称不能为空白';
+$lang['Login'] = '登入';
+$lang['Logout successful'] = '成功注销';
+$lang['Mail address'] = '电邮地址';
+$lang['Manage links'] = '连结管理';
+$lang['Manage screenshots'] = '截图管理';
+$lang['Modify extension'] = '修改扩充组件';
+$lang['Modify revision'] = '修改版本';
+$lang['Most recent extensions'] = '最新扩充组件';
+$lang['My extensions'] = '我的扩充组件';
+$lang['My personnal homepage'] = '我的个人网页';
+$lang['Name'] = '名称';
+$lang['news feed'] = '订阅 ';
+$lang['next'] = '下一步';
+$lang['No account?'] = '没有账户?';
+$lang['No extension match your filter'] = '没有符合你所筛选的扩充组件';
+$lang['No revision available for this extension.'] = '此扩充组件没有修订版本。因为此组件没有发表修订版本或没有兼容于你所筛选的版本。';
+$lang['no revision yet'] = '没有修订版本';
+$lang['Notes'] = '批注';
+$lang['Only *.zip files are allowed'] = '只容许*.zip档案';
+$lang['page generated in %s'] = '页面将在%秒后被产生';
+$lang['Password'] = '密码';
+$lang['Personnal informations'] = '个人资料';
+$lang['powered by'] = 'powered by <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = '上一步';
+$lang['Problem during upload'] = '上载过程问题';
+$lang['Rank'] = '排名';
+$lang['Register'] = '注册';
+$lang['Related links'] = '相关连结';
+$lang['Released on'] = '发表日期为';
+$lang['Reset'] = '重置';
+$lang['Revision'] = '版本';
+$lang['Revision list'] = '版本清单';
+$lang['Revision successfuly added. Thank you.'] = '成功加入修订版本,多谢!';
+$lang['Revision successfuly deleted.'] = '成功删除修订版本';
+$lang['Search'] = '搜寻';
+$lang['See extension'] = '观看扩充组件';
+$lang['Some fields are missing'] = '某些字段遗漏';
+$lang['standard view'] = '一般检视';
+$lang['Submit'] = '呈交';
+$lang['Success'] = '成功 ';
+$lang['undefined extension identifier'] = '扩充组件识别未定义';
+$lang['undefined revision identifier'] = '修订版本识别未定义';
+$lang['Username already in use'] = '使用者名称已被使用';
+$lang['Unknown extension'] = '不明扩充组件';
+$lang['URL'] = 'URL';
+$lang['Username'] = '使用名称';
+$lang['Version'] = '版本';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = '你只可以上载PNG及JPG格式的档案作为截图。';
+$lang['You did not upload anything!'] = '你没有上载任何档案!';
+$lang['You must be connected to add, modify or delete an extension.'] = '你必须在登入后, 才可新增、修改或删除扩充组件。';
+$lang['You must be connected to reach this page.'] = '你必须在登入后才可浏览此页';
+$lang['You must be the extension author to modify it.'] = '只有扩充组件的作者才可作出修改。';
+$lang['help'] = '帮助';
+$lang['You must be connected to read help user'] = '你必须在登入后才可浏览使用者帮助';
+$lang['You mustn\'t be connected to read help guest'] = '你不需要登入亦可浏览访客帮助';
+
+$lang['Language'] = '语言';
+$lang['Authors'] = '作者';
+$lang['Manage authors'] = '管理作者';
+$lang['Add an author'] = '新增作者';
+$lang['Are you sure you want to remove selected authors?'] = '你是否确认需要移除已选取的作者?';
+$lang['Delete selected users'] = '删除已选取的用户';
+$lang['You must select at least one author.'] = '你须最少选取一位作者';
+$lang['This user does not exist in database.'] = '此用户并不存在于数据库中';
+$lang['Added by']  = '新增自';
+$lang['Other extensions'] = '其他扩充组件';
+
+$lang['SVN configuration'] = 'SVN 设定';
+$lang['Repository URL'] = 'URL储存处';
+$lang['Archive root directory'] = '备存根目录';
+$lang['Archive name'] = '备存名称';
+$lang['% will be replaced by version number'] = '%将由版本编号取代';
+$lang['Delete SVN data'] = '删除 SVN 数据';
+$lang['Are you sure you want to remove SVN data?'] = '你是否确认需要移除SVN数据?';
+$lang['SVN informations'] = 'SVN 资料';
+$lang['Unable to retrieve SVN data!'] = '未能重新开启SVN数据!';
+$lang['Characters not allowed in archive root directory.'] = '备存根目录不允许使用的字符。';
+$lang['Characters not allowed in archive name.'] = '备存名称不允许使用的字符。';
+$lang['Upload a file'] = '上载档案';
+$lang['Use SVN file creation'] = '使用SVN档案创建';
+$lang['SVN revision'] = 'SVN 版本';
+$lang['An error occured during SVN export.'] = '汇出SVN时发生错误。';
+
+$lang['Available languages'] = '可用语言';
+$lang['Default description'] = '预设描述';
+$lang['Default description can not be empty'] = '预设描述不能空置';
+
+?>
+
