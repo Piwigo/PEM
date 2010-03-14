0dd47058024dffdbf29f5b2355b7f9c5c7261b6b
ddtddt <ddtddt@piwigo.org>
2010-03-14 07:23:08 +0000

    Add pack HU language thanks to sámli (http://samli.hlab.hu/)
    git-svn-id: http://piwigo.org/pem_svn/trunk@234 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/hu_HU/common.lang.php
@@ -0,0 +1,169 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'Névjegy';
+$lang['Add/Modify an extension'] = 'Bővítmény Hozzáadása/Javítása';
+$lang['Add a link'] = 'Hivatkozás hozzáadása';
+$lang['Add a revision'] = 'Változat hozzáadása';
+$lang['Add an extension'] = 'Bővítmény hozzáadása';
+$lang['Add or replace screenshot'] = 'Képernyőkép hozzáadása vagy cseréje';
+$lang['Administration'] = 'Adminisztráció';
+$lang['Agreement'] = 'Megállapodás';
+$lang['agreement_description'] = 'Tudatom teljes birtokában kijelentem, hogy hozzájárulok szellemi tulajdonom közkiccsé tételéhez, ezzel a megállapodásban foglaltakat elfogadom.';
+$lang['Are you sure you want to delete this item?'] = 'Biztos, hogy törölni akarja ezt az elemet?';
+$lang['Author'] = 'Szerző';
+$lang['Automatic redirection in %s seconds.'] = 'Automatikus átirányítás %s másodperc múlva.';
+$lang['Back to previous page'] = 'Vissza az előző oldalra';
+$lang['Can only resize PNG and JPEG files'] = 'Kizárólag PNG és JPEG fájlok átméretezése lehetséges';
+$lang['Categories'] = 'Kategóriák';
+$lang['Category'] = 'Kategória';
+$lang['Changes'] = 'Változások';
+$lang['Click here if don\'t want to wait.'] = 'Kattintson ide ha nem akarja megvárni.';
+$lang['Confirmation password does not match.'] = 'A jelszó megerősítés nem egyezik.';
+$lang['Confirm password'] = 'Jelszó megerősítése';
+$lang['compact view'] = 'kompakt nézet';
+$lang['Compatibility'] = 'Kompatibilitás';
+$lang['Compatible with'] = 'Kompatibilis';
+$lang['Connection settings'] = 'Kapcsolat beállításai';
+$lang['Deletion forbidden'] = 'Tilos törölni';
+$lang['Delete extension'] = 'Bővítmény törlés';
+$lang['Delete link'] = 'Hivatkozás törlés';
+$lang['Delete revision'] = 'Változat törlés';
+$lang['Delete screenshot'] = 'Képernyőkép törlés';
+$lang['Description'] = 'Leírás';
+$lang['Disclaimer'] = 'Felelősség kizárás';
+$lang['Disconnect'] = 'Kijelentkezés';
+$lang['Download revision'] = 'Változat letöltés';
+$lang['Downloads'] = 'Letöltés';
+$lang['Error'] = 'Hiba';
+$lang['expand/collapse all'] = 'összes kinyitása/összes bezárása';
+$lang['extensions'] = 'bővítmény';
+$lang['Extension informations'] = 'Bővebb információ';
+$lang['Extension successfuly added. Thank you.'] = 'Bővítmény hozzáadása sikeres. Köszönjük.';
+$lang['Extension successfuly deleted.'] = 'Bővítmény törlése sikeres.';
+$lang['File'] = 'Fájl';
+$lang['File too big. Filesize must not exceed %s.'] = 'A fájl túl nagy. A fájl nem lehet nagyobb mint: %s.';
+$lang['Filter'] = 'Szűrő';
+$lang['first'] = 'Első';
+$lang['First revision date'] = 'Első változat időpontja';
+$lang['Hello %s'] = 'Helló %s';
+$lang['Home'] = 'Kezdőlap';
+$lang['Identification successful'] = 'Sikeres azonosítás';
+$lang['Incorrect extension identifier'] = 'Hibás bővítmény azonosító';
+$lang['Incorrect revision identifier'] = 'Hibás változat azonosító';
+$lang['Incorrect URL'] = 'Hibás URL';
+$lang['Incorrect username'] = 'Hibás felhasználónév';
+$lang['Incorrect username/password'] = 'Hibás felhasználónév/jelszó';
+$lang['Index'] = 'Bővítmény főoldal';
+$lang['last'] = 'Utolsó';
+$lang['Latest revision date'] = 'Legutóbbi változat (dátuma)';
+$lang['Link name must not be empty'] = 'A hivatkozás neve nem lehet üres';
+$lang['Login'] = 'Belépés';
+$lang['Logout successful'] = 'Sikeres kijelentkezés';
+$lang['Mail address'] = 'E-mail cím';
+$lang['Manage links'] = 'Hivatkozások kezelése';
+$lang['Manage screenshots'] = 'Képernyőképek kezelése';
+$lang['Modify extension'] = 'Bővítmény módosítás';
+$lang['Modify revision'] = 'Változat módosítás';
+$lang['Most recent extensions'] = 'Legfrissebb bővítmények';
+$lang['My extensions'] = 'Bővítményeim';
+$lang['My personnal homepage'] = 'Személyes oldalam';
+$lang['Name'] = 'Név';
+$lang['news feed'] = 'RSS';
+$lang['next'] = 'Következő';
+$lang['No account?'] = 'Nem regisztrált?';
+$lang['No extension match your filter'] = 'Nincs találat! Próbálkozzon más szűrési feltételekkel.';
+$lang['No revision available for this extension.'] = 'Nincs elérhető javítás ehhez a bővítményhez. Vagy azért, mert nincs javítás, vagy azért, mert nem egyeztethető össze a javított verzió a beállított szűrővel.';
+$lang['no revision yet'] = 'még nincs felülvizsgálat';
+$lang['Notes'] = 'Megjegyzés';
+$lang['Only *.zip files are allowed'] = 'Csak a *. zip fájlok engedélyezettek';
+$lang['page generated in %s'] = 'az oldal %s alatt készült';
+$lang['Password'] = 'Jelszó';
+$lang['Personnal informations'] = 'Adatlap';
+$lang['powered by'] = 'powered by <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'Előző';
+$lang['Problem during upload'] = 'Probléma történt a feltöltés közben';
+$lang['Rank'] = 'Rang';
+$lang['Register'] = 'Regisztráció';
+$lang['Related links'] = 'Kapcsolódó hivatkozások';
+$lang['Released on'] = 'Feloldás bekapcsolva';
+$lang['Reset'] = 'Töröl';
+$lang['Revision'] = 'Változat';
+$lang['Revision list'] = 'Változat lista';
+$lang['Revision successfuly added. Thank you.'] = 'Változat hozzáadása sikeres. Köszönjük.';
+$lang['Revision successfuly deleted.'] = 'Változat törlése sikeres.';
+$lang['Search'] = 'Keresés';
+$lang['See extension'] = 'Bővítmény megtekintése';
+$lang['Some fields are missing'] = 'Egyes mezők hiányoznak';
+$lang['standard view'] = 'normál nézet';
+$lang['Submit'] = 'Elküld';
+$lang['Success'] = 'Siker';
+$lang['undefined extension identifier'] = 'azonosítatlan bővítmény azonosító';
+$lang['undefined revision identifier'] = 'azonosítatlan változat azonosító';
+$lang['Username already in use'] = 'A felhasználónevet már használja valaki';
+$lang['Unknown extension'] = 'Ismeretlen bővítmény';
+$lang['URL'] = 'URL';
+$lang['Username'] = 'Felhasználónév';
+$lang['Version'] = 'Verzió';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'Képernyőképnek csak PNG és JPEG fájl tölthető fel.';
+$lang['You did not upload anything!'] = 'Nem töltött fel semmit!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'Be kell lépnie egy bővítmény hozzáadásához, módosításához, vagy törléséhez.';
+$lang['You must be connected to reach this page.'] = 'Be kell lépnie az oldal eléréséhez.';
+$lang['You must be the extension author to modify it.'] = 'Be kell lépnie a bővítmény szerző módosításához.';
+$lang['help'] = 'Segítség';
+$lang['You must be connected to read help user'] = 'Belépés után olvassa el a felhasználói súgót';
+$lang['You mustn\'t be connected to read help guest'] = 'Nem lehet vendégként belépni, olvassa el a vendég súgót';
+
+$lang['Language'] = 'Nyelv';
+$lang['Authors'] = 'Szerzők';
+$lang['Manage authors'] = 'Szerzők karbantartása';
+$lang['Add an author'] = 'Szerző hozzáadás';
+$lang['Are you sure you want to remove selected authors?'] = 'Biztos, hogy törölni akarja a kiválasztott szerzőket?';
+$lang['Delete selected users'] = 'Kijelölt felhasználók törlése';
+$lang['You must select at least one author.'] = 'Legalább egy szerzőt ki kell választani.';
+$lang['This user does not exist in database.'] = 'Ez a felhasználó nincs az adatbázisban.';
+$lang['Added by']  = 'Hozzáadta';
+$lang['Other extensions'] = 'Egyéb bővítmények';
+
+$lang['SVN configuration'] = 'SVN beállítások';
+$lang['Repository URL'] = 'Repository URL';
+$lang['Archive root directory'] = 'Archívum gyökér könyvtár';
+$lang['Archive name'] = 'Archívum neve';
+$lang['% will be replaced by version number'] = '% váltja fel a verziószámot';
+$lang['Delete SVN data'] = 'SVN adatok törlése';
+$lang['Are you sure you want to remove SVN data?'] = 'Biztos, hogy törölni akarja az SVN adatokat?';
+$lang['SVN informations'] = 'SVN információk';
+$lang['Unable to retrieve SVN data!'] = 'Nem lehet letölteni az SVN adatokat!';
+$lang['Characters not allowed in archive root directory.'] = 'Nem engedélyezett karakterek az archívum gyökérkönyvtárában.';
+$lang['Characters not allowed in archive name.'] = 'Nem engedélyezett katakterek az archívum nevében.';
+$lang['Upload a file'] = 'Fájl feltöltés';
+$lang['Use SVN file creation'] = 'SVN fájl';
+$lang['SVN revision'] = 'SVN változat';
+$lang['An error occured during SVN export.'] = 'Hiba történt az SVN exportálása közben.';
+
+$lang['Available languages'] = 'Rendelkezésre álló nyelvek';
+$lang['Default description'] = 'Leírás alapértelmezett nyelve';
+$lang['Default description can not be empty'] = 'Alapértelmezett leírás nem lehet üres';
+$lang['extension'] = 'bővítmény';
+$lang['no extension'] = 'nincs bővítmény';
+
+$lang['Download last revision'] = 'Aktuális változat letöltése';
+$lang['Download'] = 'Letöltés';
+?>
