f4f5a1fce3032d88513196c94377d81d8c606cfd
ddtddt <ddtddt@piwigo.org>
2010-04-23 15:57:25 +0000

    Add NL language thanks to Kees Hessels
    git-svn-id: http://piwigo.org/pem_svn/trunk@246 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/nl_NL/common.lang.php
@@ -0,0 +1,169 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'Over';
+$lang['Add/Modify an extension'] = 'Toevoegen / Wijzigen van een uitbreiding';
+$lang['Add a link'] = 'Voeg een link toe';
+$lang['Add a revision'] = 'Voeg een aanpassing toe';
+$lang['Add an extension'] = 'Voeg een uitbreiding toe';
+$lang['Add or replace screenshot'] = 'Toevoegen of vervangen van een schermafdruk';
+$lang['Administration'] = 'Administratie';
+$lang['Agreement'] = 'Overeenkomst';
+$lang['agreement_description'] = 'Ik ga akkoord met de algemene voorwaarden die betrekking hebben op de overgang van mijn intellectuele eigendom over mijn bijdrage.';
+$lang['Are you sure you want to delete this item?'] = 'Weet u zeker dat u dit item wilt verwijderen?';
+$lang['Author'] = 'Maker';
+$lang['Automatic redirection in %s seconds.'] = 'Automatische doorverwijzing in %s seconden.';
+$lang['Back to previous page'] = 'Terug naar de vorige pagina';
+$lang['Can only resize PNG and JPEG files'] = 'Alleen PNG- en JPEG-bestanden kunnen van grootte veranderd worden';
+$lang['Categories'] = 'Categorieën';
+$lang['Category'] = 'Categorie';
+$lang['Changes'] = 'Veranderingen';
+$lang['Click here if don\'t want to wait.'] = 'Klik hier als u niet wilt wachten.';
+$lang['Confirmation password does not match.'] = 'Het bevestigde wachtwoord komt niet overeen.';
+$lang['Confirm password'] = 'Bevestig wachtwoord';
+$lang['compact view'] = 'compacte weergave';
+$lang['Compatibility'] = 'Compatibiliteit';
+$lang['Compatible with'] = 'Compatibel met';
+$lang['Connection settings'] = 'Verbindings-instellingen';
+$lang['Deletion forbidden'] = 'Verwijderen niet toegestaan';
+$lang['Delete extension'] = 'Verwijder de uitbreiding';
+$lang['Delete link'] = 'Verwijder de link';
+$lang['Delete revision'] = 'Verwijder de aanpassing';
+$lang['Delete screenshot'] = 'Verwijder de schermafdruk';
+$lang['Description'] = 'Beschrijving';
+$lang['Disclaimer'] = 'Verantwoordelijkheid';
+$lang['Disconnect'] = 'Verbinding verbreken';
+$lang['Download revision'] = 'Download de aanpassing';
+$lang['Downloads'] = 'Downloads';
+$lang['Error'] = 'Fout';
+$lang['expand/collapse all'] = 'alles inklappen/uitklappen';
+$lang['extensions'] = 'uitbreidingen';
+$lang['Extension informations'] = 'Informatie over de uitbreiding';
+$lang['Extension successfuly added. Thank you.'] = 'De uitbreiding is succesvol toegevoegd. Bedankt.';
+$lang['Extension successfuly deleted.'] = 'De uitbreiding is succesvol verwijderd.';
+$lang['File'] = 'Bestand';
+$lang['File too big. Filesize must not exceed %s.'] = 'Bestand te groot. Het bestand mag niet groter zijn dan %s.';
+$lang['Filter'] = 'Filter';
+$lang['first'] = 'eerste';
+$lang['First revision date'] = 'Datum van de eerste aanpassing';
+$lang['Hello %s'] = 'Hallo %s';
+$lang['Home'] = 'Start';
+$lang['Identification successful'] = 'Identificatie geslaagd';
+$lang['Incorrect extension identifier'] = 'Onjuiste uitbreidings-identificatie';
+$lang['Incorrect revision identifier'] = 'Onjuiste aanpassings-identificatie';
+$lang['Incorrect URL'] = 'Onjuiste URL';
+$lang['Incorrect username'] = 'Onjuiste gebruikersnaam';
+$lang['Incorrect username/password'] = 'Onjuiste gebruikersnaam / onjuist wachtwoord';
+$lang['Index'] = 'Index';
+$lang['last'] = 'Laatste';
+$lang['Latest revision date'] = 'Laatste aanpassings-datum';
+$lang['Link name must not be empty'] = 'De linknaam mag niet leeg zijn';
+$lang['Login'] = 'Aanmelden';
+$lang['Logout successful'] = 'Afmelden succesvol';
+$lang['Mail address'] = 'Email-adres';
+$lang['Manage links'] = 'Beheer links';
+$lang['Manage screenshots'] = 'Beheer schermafdrukken';
+$lang['Modify extension'] = 'Wijzig de uitbreiding';
+$lang['Modify revision'] = 'Wijzig de aanpassing';
+$lang['Most recent extensions'] = 'Mest recente uitbreidingen';
+$lang['My extensions'] = 'Mijn uitbreidingern';
+$lang['My personnal homepage'] = 'Mijn persoonlijke homepage';
+$lang['Name'] = 'Naam';
+$lang['news feed'] = 'nieuws-feed';
+$lang['next'] = 'Volgende';
+$lang['No account?'] = 'Nog geen account?';
+$lang['No extension match your filter'] = 'Geen enkele uitbreiding komt overeen met uw filter';
+$lang['No revision available for this extension.'] = 'Er is geen aanpssing beschikbaar voor deze uitbreiding. Of er is geen enkele aanpassing beschikbaar of er is geen aanpassing die voldoet aan de door u ingestelde filter.';
+$lang['no revision yet'] = 'nog geen aanpassingen';
+$lang['Notes'] = 'Aantekeningen';
+$lang['Only *.zip files are allowed'] = 'Alleen *.zip bestanden zijn toegestaan';
+$lang['page generated in %s'] = 'pagina gegenereerd in %s';
+$lang['Password'] = 'Wachtwoord';
+$lang['Personnal informations'] = 'Persoonlijke informatie';
+$lang['powered by'] = 'powered by <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'Vorige';
+$lang['Problem during upload'] = 'Probleem tijdens de upload';
+$lang['Rank'] = 'Rang';
+$lang['Register'] = 'Register';
+$lang['Related links'] = 'Gerelateerde links';
+$lang['Released on'] = 'Uitgebracht op';
+$lang['Reset'] = 'Reset';
+$lang['Revision'] = 'Aanpassing';
+$lang['Revision list'] = 'Aanpassingenlijst';
+$lang['Revision successfuly added. Thank you.'] = 'Aanpassing succesvol toegevoegd. Bedankt.';
+$lang['Revision successfuly deleted.'] = 'Aanpassing succesvol verwijderd.';
+$lang['Search'] = 'Zoeken';
+$lang['See extension'] = 'Bekijk de uitbreiding';
+$lang['Some fields are missing'] = 'Somige velden zijn niet ingevuld';
+$lang['standard view'] = 'standaard scherm';
+$lang['Submit'] = 'Verstuur';
+$lang['Success'] = 'Succes';
+$lang['undefined extension identifier'] = 'ongedefinieerde uitbreidings-identificatie';
+$lang['undefined revision identifier'] = 'ongedefinieerde aanpassings-identificatie';
+$lang['Username already in use'] = 'Gebruikersnaam is reeds in gebruik';
+$lang['Unknown extension'] = 'Onbekende uitbreiding';
+$lang['URL'] = 'URL';
+$lang['Username'] = 'Gebruikersnaam';
+$lang['Version'] = 'Versie';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'U kunt alleen PNG- en JPEG-bestanden als schermafdruk uploaden.';
+$lang['You did not upload anything!'] = 'U heeft niets ge-upload!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'U moet verbonden zijn om een uitbreiding te kunnen toevoegen, wijzigen of verwijderen.';
+$lang['You must be connected to reach this page.'] = 'U moet verbonden zijn om deze pagina te kunnen bereiken.';
+$lang['You must be the extension author to modify it.'] = 'U moet de maker van de uitbreiding zijn om deze te kunnen wijzigen.';
+$lang['help'] = 'Help';
+$lang['You must be connected to read help user'] = 'U moet verbonden zijn om de gebruikers-help te kunnen lezen';
+$lang['You mustn\'t be connected to read help guest'] = 'U hoeft niet verbonden te zijn om de gast-help te kunnen lezen';
+
+$lang['Language'] = 'Taal';
+$lang['Authors'] = 'Makers';
+$lang['Manage authors'] = 'Beheer makers';
+$lang['Add an author'] = 'Voeg een maker toe';
+$lang['Are you sure you want to remove selected authors?'] = 'Weet u zeker dat u de geselecteerde makers wilt verwijderen?';
+$lang['Delete selected users'] = 'Verwijder de geselecteerde gebruikers';
+$lang['You must select at least one author.'] = 'U moet minstens één maker selecteren.';
+$lang['This user does not exist in database.'] = 'Deze gebruiker bestaat niet in de database.';
+$lang['Added by']  = 'Toegevoegd door';
+$lang['Other extensions'] = 'Andere uitbreidingen';
+
+$lang['SVN configuration'] = 'SVN configuratie';
+$lang['Repository URL'] = 'Repository URL';
+$lang['Archive root directory'] = 'Archief root directory';
+$lang['Archive name'] = 'Archiefnaam';
+$lang['% will be replaced by version number'] = '% wordt vervangen door het versie nummer';
+$lang['Delete SVN data'] = 'Verwijder SVN gegevens';
+$lang['Are you sure you want to remove SVN data?'] = 'Weet u zeker dat de SVN gegevens verwijderd kunnen worden?';
+$lang['SVN informations'] = 'SVN informatie';
+$lang['Unable to retrieve SVN data!'] = 'Niet mogelijk om SVN gegevens op te halen';
+$lang['Characters not allowed in archive root directory.'] = 'Lettertekens niet toegestaan in de archief root directory.';
+$lang['Characters not allowed in archive name.'] = 'Lettertekens niet toegestaan in de archiefnaam.';
+$lang['Upload a file'] = 'Upload een bestand';
+$lang['Use SVN file creation'] = 'Gebruik SVN-bestandsaanmaak';
+$lang['SVN revision'] = 'SVN-aanpassing';
+$lang['An error occured during SVN export.'] = 'Er is iets fout gegaan tijdens de SVN export.';
+
+$lang['Available languages'] = 'Beschikbare talen';
+$lang['Default description'] = 'Standaard beschrijving';
+$lang['Default description can not be empty'] = 'De standaard beschrijving mag niet leeg zijn';
+$lang['extension'] = 'uitbreiding';
+$lang['no extension'] = 'geen uitbreiding';
+
+$lang['Download last revision'] = 'Download de laatste aanpassing';
+$lang['Download'] = 'Download';
+?>
