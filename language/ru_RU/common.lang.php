57fd5aed63477d1381594d20ca732e1f2042959f
ddtddt <ddtddt@piwigo.org>
2010-09-17 15:52:26 +0000

    Add RU language thanks to IE
    git-svn-id: http://piwigo.org/pem_svn/trunk@251 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/ru_RU/common.lang.php
@@ -0,0 +1,169 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'Описание';
+$lang['Add/Modify an extension'] = 'Добавить/Изменить дополнение';
+$lang['Add a link'] = 'Добавить ссылку';
+$lang['Add a revision'] = 'Добавить отзыв';
+$lang['Add an extension'] = 'Добавить дополнение';
+$lang['Add or replace screenshot'] = 'Добавить или заменить скриншот';
+$lang['Administration'] = 'Администрирование';
+$lang['Agreement'] = 'Соглашение';
+$lang['agreement_description'] = 'Я принимаю эти условия соглашения, которые предусматривают прием моей интеллектуальной собственности в качестве добровольного вклада.';
+$lang['Are you sure you want to delete this item?'] = 'Вы уверены, что хотите удалить это?';
+$lang['Author'] = 'Автор';
+$lang['Automatic redirection in %s seconds.'] = 'Автоматическое перенаправление через %s секунд.';
+$lang['Back to previous page'] = 'К предыдущей странице';
+$lang['Can only resize PNG and JPEG files'] = 'Можно менять размер только у файлов в форматах PNG и JPEG';
+$lang['Categories'] = 'Категории';
+$lang['Category'] = 'Категория';
+$lang['Changes'] = 'Изменения';
+$lang['Click here if don\'t want to wait.'] = 'Кликните сюда, если не хотите ждать.';
+$lang['Confirmation password does not match.'] = 'Пароли не совпадают';
+$lang['Confirm password'] = 'Подтвердите пароль';
+$lang['compact view'] = 'компактный вид';
+$lang['Compatibility'] = 'Совместимость';
+$lang['Compatible with'] = 'Совместимо с';
+$lang['Connection settings'] = 'Настройки соединения';
+$lang['Deletion forbidden'] = 'Удаление запрещено';
+$lang['Delete extension'] = 'Удалить дополнение';
+$lang['Delete link'] = 'Удалить ссылку';
+$lang['Delete revision'] = 'Удалить отзыв';
+$lang['Delete screenshot'] = 'Удалить скриншот';
+$lang['Description'] = 'Описание';
+$lang['Disclaimer'] = 'Правовая оговорка';
+$lang['Disconnect'] = 'Отсоединиться';
+$lang['Download revision'] = 'Скачать обновление';
+$lang['Downloads'] = 'Загрузки';
+$lang['Error'] = 'Ошибка';
+$lang['expand/collapse all'] = 'развернуть/свернуть все';
+$lang['extensions'] = 'дополнения';
+$lang['Extension informations'] = 'Информация о дополнении';
+$lang['Extension successfuly added. Thank you.'] = 'Дополнение успешно установлено. Благодарим вас';
+$lang['Extension successfuly deleted.'] = 'Дополнение успешно удалено.';
+$lang['File'] = 'Файл';
+$lang['File too big. Filesize must not exceed %s.'] = 'Файл слишком велик. Размер файла не должен превышать %s.';
+$lang['Filter'] = 'Фильтр';
+$lang['first'] = 'Первое';
+$lang['First revision date'] = 'Дата первого пересмотра';
+$lang['Hello %s'] = 'Здравствуйте, %s';
+$lang['Home'] = 'Главная';
+$lang['Identification successful'] = 'Идентификация прошла успешно';
+$lang['Incorrect extension identifier'] = 'неверный идентификатор дополнения';
+$lang['Incorrect revision identifier'] = 'Неверный идентификатор пересмотра';
+$lang['Incorrect URL'] = 'Неверный веб-адрес';
+$lang['Incorrect username'] = 'Неверное имя пользователя';
+$lang['Incorrect username/password'] = 'Неверное имя пользователя/пароль';
+$lang['Index'] = 'Индекс';
+$lang['last'] = 'Последнее';
+$lang['Latest revision date'] = 'Самая поздняя дата пересмотра';
+$lang['Link name must not be empty'] = 'Место ссылки не должно оставаться пустым';
+$lang['Login'] = 'Логин';
+$lang['Logout successful'] = 'Вы вышли';
+$lang['Mail address'] = 'Адрес почты';
+$lang['Manage links'] = 'Управление ссылками';
+$lang['Manage screenshots'] = 'Управление скриншотами';
+$lang['Modify extension'] = 'Изменить дополнение';
+$lang['Modify revision'] = 'Изменить пересмотр';
+$lang['Most recent extensions'] = 'Самые новые дополнения';
+$lang['My extensions'] = 'Мои дополнения';
+$lang['My personnal homepage'] = 'Моя домашняя страница';
+$lang['Name'] = 'Имя';
+$lang['news feed'] = 'поток новостей';
+$lang['next'] = 'Следующий';
+$lang['No account?'] = 'Нет аккаунта?';
+$lang['No extension match your filter'] = 'Нет дополнений, отвечающих вашему фильтру';
+$lang['No revision available for this extension.'] = 'Для этого дополнения не существует ревизий. Или нет ревизий вообще, или нет ревизий, подходящих к указанной вами версии.';
+$lang['no revision yet'] = 'пока нет ревизии';
+$lang['Notes'] = 'Заметки';
+$lang['Only *.zip files are allowed'] = 'Только *.zip файлы';
+$lang['page generated in %s'] = 'страница сгенерирована за %s с';
+$lang['Password'] = 'Пароль';
+$lang['Personnal informations'] = 'Личная информация';
+$lang['powered by'] = 'обеспечивается <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'Пред.';
+$lang['Problem during upload'] = 'Проблема во время загрузки';
+$lang['Rank'] = 'Ранг';
+$lang['Register'] = 'Зрагистрируйтесь';
+$lang['Related links'] = 'Связанные ссылки';
+$lang['Released on'] = 'Выпущено';
+$lang['Reset'] = 'Сброс';
+$lang['Revision'] = 'Ревизия';
+$lang['Revision list'] = 'Список ревизий';
+$lang['Revision successfuly added. Thank you.'] = 'Ревизия успешно добалвена. Спасибо.';
+$lang['Revision successfuly deleted.'] = 'Ревизия успешно удалена.';
+$lang['Search'] = 'Поиск';
+$lang['See extension'] = 'Посмотреть дополнение';
+$lang['Some fields are missing'] = 'Некоторые поля не заполнены';
+$lang['standard view'] = 'стандартный вид';
+$lang['Submit'] = 'Отправить';
+$lang['Success'] = 'Успешно';
+$lang['undefined extension identifier'] = 'неопределенный идентификатор дополнения';
+$lang['undefined revision identifier'] = 'неопределенный идентификатор ревизии';
+$lang['Username already in use'] = 'Это имя пользователя занято';
+$lang['Unknown extension'] = 'Неизвестное дополнение';
+$lang['URL'] = 'URL';
+$lang['Username'] = 'Имя пользователя';
+$lang['Version'] = 'Версия';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'В качестве скриншота вы можете загружать только JPG или PNG.';
+$lang['You did not upload anything!'] = 'Вы ничего не загрузили!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'You must be connected to add, modify or delete an extension.';
+$lang['You must be connected to reach this page.'] = 'Для просмотра этой страницы вы должны быть подсоединены.';
+$lang['You must be the extension author to modify it.'] = 'Для редактирования вы должны быть автором дополнения.';
+$lang['help'] = 'Помощь';
+$lang['You must be connected to read help user'] = 'Вы должны быть подсоединены чтобы прочитать справку, пользователь';
+$lang['You mustn\'t be connected to read help guest'] = 'Вы должны быть подсоединены чтобы прочитать справку, гость';
+
+$lang['Language'] = 'Язык';
+$lang['Authors'] = 'Авторы';
+$lang['Manage authors'] = 'Управление авторами';
+$lang['Add an author'] = 'Добавить автора';
+$lang['Are you sure you want to remove selected authors?'] = 'Вы уверены, что хотите удалить выбранных авторов?';
+$lang['Delete selected users'] = 'Удалить выбранных пользователей';
+$lang['You must select at least one author.'] = 'Нужно выбрать по крайней мере одного автора.';
+$lang['This user does not exist in database.'] = 'Этого пользователя нет в базе данных.';
+$lang['Added by']  = 'Добавлено';
+$lang['Other extensions'] = 'Другие дополнения';
+
+$lang['SVN configuration'] = 'Конфигурация SVN ';
+$lang['Repository URL'] = 'URL репозитория';
+$lang['Archive root directory'] = 'Корневая директория архива';
+$lang['Archive name'] = 'Имя архива';
+$lang['% will be replaced by version number'] = '% будет заменено на номер версии';
+$lang['Delete SVN data'] = 'Удалить данные SVN';
+$lang['Are you sure you want to remove SVN data?'] = 'вы действительно хотите удалить данные SVN?';
+$lang['SVN informations'] = 'SVN информация';
+$lang['Unable to retrieve SVN data!'] = 'Невозможно извлечь данные SVN!';
+$lang['Characters not allowed in archive root directory.'] = 'Эти символы нельзя использовать в корневой директории.';
+$lang['Characters not allowed in archive name.'] = 'Эти символы нельзя использовать в имени архива.';
+$lang['Upload a file'] = 'Закачать файл';
+$lang['Use SVN file creation'] = 'Используйте создание файла SVN';
+$lang['SVN revision'] = 'Ревизия SVN';
+$lang['An error occured during SVN export.'] = 'Во время экспорта SVN произошла ошибка.';
+
+$lang['Available languages'] = 'Доступные языки';
+$lang['Default description'] = 'Описание по умолчанию';
+$lang['Default description can not be empty'] = 'Описание по умолчанию не может оставаться пустым';
+$lang['extension'] = 'дополнение';
+$lang['no extension'] = 'нет дополнения';
+
+$lang['Download last revision'] = 'Скачать самую свежую ревизию';
+$lang['Download'] = 'Скачать';
+?>
