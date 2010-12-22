74654134efcaf2f1949f0e7f860bc7075c4f7001
ddtddt <ddtddt@piwigo.org>
2010-12-22 16:04:05 +0000

    Add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/pem_svn/trunk@257 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/lv_LV/common.lang.php
@@ -0,0 +1,169 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'Par';
+$lang['Add/Modify an extension'] = 'Pievienot / Modificēt paplašinājumu';
+$lang['Add a link'] = 'Pievienot saiti';
+$lang['Add a revision'] = 'Pievienot revidēto izdevumu';
+$lang['Add an extension'] = 'Pievienot paplašinājumu';
+$lang['Add or replace screenshot'] = 'Pievienot vai mainīt ekrānuzņēmumu';
+$lang['Administration'] = 'Administrēšana';
+$lang['Agreement'] = 'Vienošanās';
+$lang['agreement_description'] = 'Es pieņemu šo noteikumu un nosacījumu nolīgumu, kur atrunāti mana intelektuālā īpašuma izmantošana.';
+$lang['Are you sure you want to delete this item?'] = 'Vai esat pārliecināti, ka gribat dzēst šo vienību?';
+$lang['Author'] = 'Autors';
+$lang['Automatic redirection in %s seconds.'] = 'Automātiskā pāradresācija pēc %s sekundēm.';
+$lang['Back to previous page'] = 'Atpakaļ uz iepriekšējo lapu';
+$lang['Can only resize PNG and JPEG files'] = 'Iespējams mainīt izmērus tikai PNG un JPEG failiem';
+$lang['Categories'] = 'Kategorijas';
+$lang['Category'] = 'Kategorija';
+$lang['Changes'] = 'Izmaiņas';
+$lang['Click here if don\'t want to wait.'] = 'Klikšķiniet šeit, ja negribat gaidīt.';
+$lang['Confirmation password does not match.'] = 'Apstiprinājuma parole nesakrīt.';
+$lang['Confirm password'] = 'Apstiprināt paroli';
+$lang['compact view'] = 'kompaktskats';
+$lang['Compatibility'] = 'Saderība';
+$lang['Compatible with'] = 'Saderīgs ar';
+$lang['Connection settings'] = 'Savienojuma uzstādījumi';
+$lang['Deletion forbidden'] = 'Dzēst aizliegts';
+$lang['Delete extension'] = 'Dzēst paplašinājumu';
+$lang['Delete link'] = 'Dzēst saiti';
+$lang['Delete revision'] = 'Dzēst revidēto izdevumu';
+$lang['Delete screenshot'] = 'Dzēst ekrānuzņemumu';
+$lang['Description'] = 'Apraksts';
+$lang['Disclaimer'] = 'Atruna';
+$lang['Disconnect'] = 'Atvienot';
+$lang['Download revision'] = 'Lejupielādēt revidēto izdevumu';
+$lang['Downloads'] = 'Lejuplādes';
+$lang['Error'] = 'Kļūda';
+$lang['expand/collapse all'] = 'izvērst/sakļaut visu';
+$lang['extensions'] = 'paplašinājumi';
+$lang['Extension informations'] = 'Informācija par paplašinājumiem';
+$lang['Extension successfuly added. Thank you.'] = 'Paplašinājums sekmīgi pievienots. Paldies.';
+$lang['Extension successfuly deleted.'] = 'Paplašinājums sekmīgi dzēsts.';
+$lang['File'] = 'Fails';
+$lang['File too big. Filesize must not exceed %s.'] = 'Fails ir par lielu. Faila izmērs nedrīkst pārsniegt %s.';
+$lang['Filter'] = 'Filtrēt';
+$lang['first'] = 'Pirmais';
+$lang['First revision date'] = 'Pirmās revīzijas datums';
+$lang['Hello %s'] = 'Sveiki %s';
+$lang['Home'] = 'Mājas';
+$lang['Identification successful'] = 'Identifikācija veiksmīga';
+$lang['Incorrect extension identifier'] = 'Nekorekts paplašinājuma identifikators';
+$lang['Incorrect revision identifier'] = 'Nekorekts revīzijas identifikators';
+$lang['Incorrect URL'] = 'Nekorekts URL';
+$lang['Incorrect username'] = 'Nekorekts lietotājvārds';
+$lang['Incorrect username/password'] = 'Nekorekts lietotājvārds/parole';
+$lang['Index'] = 'Indekss';
+$lang['last'] = 'Pēdējais';
+$lang['Latest revision date'] = 'Pēdējās revīzijas datums';
+$lang['Link name must not be empty'] = 'Saites nosaukums nedrīkst būt tukšs';
+$lang['Login'] = 'Ielogoties';
+$lang['Logout successful'] = 'Iziešana no sistēmas veiksmīga';
+$lang['Mail address'] = 'Pasta adrese';
+$lang['Manage links'] = 'Pārvaldīt saites';
+$lang['Manage screenshots'] = 'Pārvaldīt ekrānuzņēmumus';
+$lang['Modify extension'] = 'Modificēt paplašinājumu';
+$lang['Modify revision'] = 'Modificēt revīziju';
+$lang['Most recent extensions'] = 'Pēdējie paplašinājumi';
+$lang['My extensions'] = 'Mani paplašinājumi';
+$lang['My personnal homepage'] = 'Mana personīgā mājas lapa';
+$lang['Name'] = 'Nosaukums';
+$lang['news feed'] = 'ziņu piegāde';
+$lang['next'] = 'Nākošais';
+$lang['No account?'] = 'Nav konta?';
+$lang['No extension match your filter'] = 'Nav jūsu filtram atbilstoša paplašinājuma';
+$lang['No revision available for this extension.'] = 'Nav atbilstošas revīzijas jūsu paplašinājumam. Iespējams, ka izdevuma revīzijas nav vispār, vai arī nav ar jūsu filtrā iestatītās versijas numuru saderīgu revīziju.';
+$lang['no revision yet'] = 'revīziju vēl nav';
+$lang['Notes'] = 'Piezīmes';
+$lang['Only *.zip files are allowed'] = 'Atļauti tikai *.zip tipa faili';
+$lang['page generated in %s'] = 'lapa izveidota %s';
+$lang['Password'] = 'Parole';
+$lang['Personnal informations'] = 'Personālā informācija';
+$lang['powered by'] = 'nodrošināta ar <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'Iepriekšējais';
+$lang['Problem during upload'] = 'Problēmas augšupielādējot';
+$lang['Rank'] = 'Kategorija';
+$lang['Register'] = 'Reģistrs';
+$lang['Related links'] = 'Saistītās saites';
+$lang['Released on'] = 'Izdots';
+$lang['Reset'] = 'Atjaunot';
+$lang['Revision'] = 'Revīzija';
+$lang['Revision list'] = 'Revīziju sarakstst';
+$lang['Revision successfuly added. Thank you.'] = 'Revīzija sekmīgi pievienota. Paldies.';
+$lang['Revision successfuly deleted.'] = 'Revīzija sekmīgi dzēsta.';
+$lang['Search'] = 'Meklēt';
+$lang['See extension'] = 'Skatīt paplašinājumu';
+$lang['Some fields are missing'] = 'Iztrūkst daži lauki';
+$lang['standard view'] = 'standartskats';
+$lang['Submit'] = 'Iesniegt';
+$lang['Success'] = 'Veiksme';
+$lang['undefined extension identifier'] = 'nedefinēts paplašinājuma identifikators';
+$lang['undefined revision identifier'] = 'nedefinēts revīzijas identifikators';
+$lang['Username already in use'] = 'Lietotājvārds jau tiek izmantots';
+$lang['Unknown extension'] = 'Nezināms paplašinājums';
+$lang['URL'] = 'URL';
+$lang['Username'] = 'Lietotājvārds';
+$lang['Version'] = 'Versija';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'Kā ekrānuzņēmumu jūs varat augšuplādēt tikai PNG un JPEG failus.';
+$lang['You did not upload anything!'] = 'Jūs neko neesat augšupielādējis!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'Jums jābūt savienojumā, lai pievienotu, modificētu vai dzēstu paplašinājumu.';
+$lang['You must be connected to reach this page.'] = 'Jums jābūt savienojumā, lai tiktu pie šīs lapas.';
+$lang['You must be the extension author to modify it.'] = 'Jums jābūt paplašinājuma autoram, lai to modificētu.';
+$lang['help'] = 'Palīdzība';
+$lang['You must be connected to read help user'] = 'Jums jābūt savienojumā, lai lasītu palīdzību lietotājiem';
+$lang['You mustn\'t be connected to read help guest'] = 'Jums nav jābūt savienojumā, lai lasītu palīdzību viesiem';
+
+$lang['Language'] = 'Valoda';
+$lang['Authors'] = 'Autors';
+$lang['Manage authors'] = 'Pārvaldīt autorus';
+$lang['Add an author'] = 'Pievienot autoru';
+$lang['Are you sure you want to remove selected authors?'] = 'Vai esat pārliecināti, ka gribat dzēst atlasītos autorus?';
+$lang['Delete selected users'] = 'Dzēst atlasītos lietotājus';
+$lang['You must select at least one author.'] = 'Jums jāizvēlas vismaz viens autors.';
+$lang['This user does not exist in database.'] = 'Šā lietotāja datu bāzē nav.';
+$lang['Added by']  = 'Pievienojis';
+$lang['Other extensions'] = 'Citi paplašinājumi';
+
+$lang['SVN configuration'] = 'SVN konfigurācija';
+$lang['Repository URL'] = 'Glabātuves URL';
+$lang['Archive root directory'] = 'Arhīva saknes mape';
+$lang['Archive name'] = 'Arhīva nosaukums';
+$lang['% will be replaced by version number'] = '% tiks nomainīts ar ar versijas numuru';
+$lang['Delete SVN data'] = 'Dzēst SVN datus';
+$lang['Are you sure you want to remove SVN data?'] = 'Vai esat pārliecināti, ka gribat dzēst SVN datus?';
+$lang['SVN informations'] = 'SVN informācija';
+$lang['Unable to retrieve SVN data!'] = 'Nav iespējams iegūt SVN datus!';
+$lang['Characters not allowed in archive root directory.'] = 'Rakstzīmes nav atļautas arhīva saknes mapē.';
+$lang['Characters not allowed in archive name.'] = 'Rakstzīmes nav atļautas arhīva nosaukumā.';
+$lang['Upload a file'] = 'Augšuplādēt failu';
+$lang['Use SVN file creation'] = 'Izmantot SVN failu izveidi';
+$lang['SVN revision'] = 'SVN revīzija';
+$lang['An error occured during SVN export.'] = 'Radās kļūda SVN eksporta laikā.';
+
+$lang['Available languages'] = 'Pieejamās valodas';
+$lang['Default description'] = 'Apraksts pēc noklusējuma';
+$lang['Default description can not be empty'] = 'Apraksts pēc noklusējuma nedrīkst būt tukšs';
+$lang['extension'] = 'paplašinājums';
+$lang['no extension'] = 'bez paplašinājuma';
+
+$lang['Download last revision'] = 'Lejupielādēt pēdējo revīzijas versiju';
+$lang['Download'] = 'Lejuplādēt';
+?>
