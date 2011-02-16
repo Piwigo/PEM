addc40df5500279c8aa1ad87d7f16c4d10f7d353
ddtddt <ddtddt@piwigo.org>
2011-02-16 14:26:19 +0000

    Add no_NO (Norwegian) thanks to atlew
    git-svn-id: http://piwigo.org/pem_svn/trunk@266 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/no_NO/common.lang.php
@@ -0,0 +1,170 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'Om';
+$lang['Add/Modify an extension'] = 'Legg til /Endre en utvidelse'; 
+$lang['Add a link'] = 'Legg til en link'; 
+$lang['Add a revision'] = 'Legg til en revisjon'; 
+$lang['Add an extension'] = 'Legg til en utvidelse'; 
+$lang['Add or replace screenshot'] = 'Legg til eller erstatt et skjermbilde'; 
+$lang['Administration'] = 'Administrasjon'; 
+$lang['Agreement'] = 'Forståelse'; 
+$lang['agreement_description'] = 'Jeg aksepterer disse vilkår og avtaler om at mitt åndsverk blir gjengitt dedikert til meg.'; 
+$lang['Are you sure you want to delete this item?'] = 'Er du sikker på at du vil slette dette elementet?'; 
+$lang['Author'] = 'Forfatter'; 
+$lang['Automatic redirection in %s seconds.'] = 'Automatisk videresending om %s sekunder.'; 
+$lang['Back to previous page'] = 'Tilbake til forrige side'; 
+$lang['Can only resize PNG and JPEG files'] = 'Kan bare endre størrelse på PNG og JPEG filer'; 
+$lang['CAPTCHA_error'] = 'Ordene du har skrevet matcher ikke. Vennligst prøv på nytt.'; 
+$lang['Categories'] = 'Album'; 
+$lang['Category'] = 'Album'; 
+$lang['Changes'] = 'Endringer'; 
+$lang['Click here if don\'t want to wait.'] = 'Klikk her om du ikke vil vente.'; 
+$lang['Confirmation password does not match.'] = 'Bekreftelses passordet er ikke likt.'; 
+$lang['Confirm password'] = 'Bekreft passord'; 
+$lang['compact view'] = 'Kompakt visning'; 
+$lang['Compatibility'] = 'Kompatibilitet'; 
+$lang['Compatible with'] = 'Kompatibel med'; 
+$lang['Connection settings'] = 'Tilkoblings innstillinger'; 
+$lang['Deletion forbidden'] = 'Sletting forbudt'; 
+$lang['Delete extension'] = 'Slett tillegg'; 
+$lang['Delete link'] = 'Slett link'; 
+$lang['Delete revision'] = 'Slett revisjon'; 
+$lang['Delete screenshot'] = 'Slett skjermbilde'; 
+$lang['Description'] = 'Beskrivelse'; 
+$lang['Disclaimer'] = 'Forståelse'; 
+$lang['Disconnect'] = 'Koble ifra'; 
+$lang['Download revision'] = 'Last ned revisjon'; 
+$lang['Downloads'] = 'Nedlastinger'; 
+$lang['Error'] = 'Feil'; 
+$lang['expand/collapse all'] = 'Utvid/vis alle'; 
+$lang['extensions'] = 'utvidelser'; 
+$lang['Extension informations'] = 'Utvidelse informasjon'; 
+$lang['Extension successfuly added. Thank you.'] = 'Utvidelse er lagt til. Takk for det.'; 
+$lang['Extension successfuly deleted.'] = 'Utvidelse er slettet.'; 
+$lang['File'] = 'Fil'; 
+$lang['File too big. Filesize must not exceed %s.'] = 'Filen er for stor. Filestørrelse må ikke overstige %s.';
+$lang['Filter'] = 'Filter';
+$lang['first'] = 'Første';
+$lang['First revision date'] = 'Første revisjon dato'; 
+$lang['Hello %s'] = 'Hallo %s'; 
+$lang['Home'] = 'Hjem'; 
+$lang['Identification successful'] = 'Identifikasjon vellykket'; 
+$lang['Incorrect extension identifier'] = 'Feil utvidelse identifiaktor'; 
+$lang['Incorrect revision identifier'] = 'Feil revisjon identifiaktor'; 
+$lang['Incorrect URL'] = 'Feil URL'; 
+$lang['Incorrect username'] = 'Feil brukernavn'; 
+$lang['Incorrect username/password'] = 'Feil brukernavn/passord'; 
+$lang['Index'] = 'Index'; 
+$lang['last'] = 'Siste'; 
+$lang['Latest revision date'] = 'Siste revisjon dato'; 
+$lang['Link name must not be empty'] = 'Link navn kan ikke være tom'; 
+$lang['Login'] = 'Loggin'; 
+$lang['Logout successful'] = 'Logget ut med suksess'; 
+$lang['Mail address'] = 'Mail addresse'; 
+$lang['Manage links'] = 'Behandle linker'; 
+$lang['Manage screenshots'] = 'Behandle skjermbilder'; 
+$lang['Modify extension'] = 'Endre utvidelser'; 
+$lang['Modify revision'] = 'Endre revisjon'; 
+$lang['Most recent extensions'] = 'De siste utvidelser'; 
+$lang['My extensions'] = 'Mine utvidelser.'; 
+$lang['My personnal homepage'] = 'Min personlige hjemme side'; 
+$lang['Name'] = 'Navn'; 
+$lang['news feed'] = 'nyhets mating'; 
+$lang['next'] = 'Neste'; 
+$lang['No account?'] = 'Ingen konto?'; 
+$lang['No extension match your filter'] = 'Ingen utvidelser passer til filtreringen'; 
+$lang['No revision available for this extension.'] = 'Ingen revisjon tilgjengelig for denne utvidelsen. Enten fordi det ikke er noen revisjon i det hele tatt, eller fordi det ikke er noen revisjoner som er komptible med versjon filteret du har satt.';
+$lang['no revision yet'] = 'ingen revisjon enda'; 
+$lang['Notes'] = 'Notater'; 
+$lang['Only *.zip files are allowed'] = 'Kun *.zip filer er tillatt'; 
+$lang['page generated in %s'] = 'side generert på %s'; 
+$lang['Password'] = 'Passord'; 
+$lang['Personnal informations'] = 'Personlig informasjon'; 
+$lang['powered by'] = 'støttet av <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'Forrige';
+$lang['Problem during upload'] = 'Problem ved opplasting'; 
+$lang['Rank'] = 'Rank'; 
+$lang['Register'] = 'Registrer'; 
+$lang['Related links'] = 'Relaterte linker'; 
+$lang['Released on'] = 'Sluppet den'; 
+$lang['Reset'] = 'Reset'; 
+$lang['Revision'] = 'Revisjon'; 
+$lang['Revision list'] = 'Revisjon liste'; 
+$lang['Revision successfuly added. Thank you.'] = 'Revisjon suksessfult lagt til. Takk for det.'; 
+$lang['Revision successfuly deleted.'] = 'Revisjon suksessfult slettet.'; 
+$lang['Search'] = 'Søk'; 
+$lang['See extension'] = 'Se utvidelser'; 
+$lang['Some fields are missing'] = 'Noen felter mangler'; 
+$lang['standard view'] = 'standard visning'; 
+$lang['Submit'] = 'Lagre'; 
+$lang['Success'] = 'Vellykket'; 
+$lang['undefined extension identifier'] = 'udefinert utvidelse identifiserer'; 
+$lang['undefined revision identifier'] = 'udefinert revisjon identifiserer';
+$lang['Username already in use'] = 'Brukernavn er i bruk allerede'; 
+$lang['Unknown extension'] = 'Ukjent utvidelse'; 
+$lang['URL'] = 'URL'; 
+$lang['Username'] = 'Brukernavn'; 
+$lang['Version'] = 'Versjon'; 
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'Du kan kun laste opp PNG og JPEG filer som skjermbilder.'; 
+$lang['You did not upload anything!'] = 'Du lastet ikke opp noen ting!'; 
+$lang['You ust be connected to add, modify or delete an extension.'] = 'Du må være koblet til for å legge til, endre eller slette en utvidelse.'; 
+$lang['You must be connected to reach this page.'] = 'Du må være koblet til for å nå denne siden.'; 
+$lang['You must be the extension author to modify it.'] = 'Du må være utvidelsens forfatter for å endre den.'; 
+$lang['help'] = 'Hjelp'; 
+$lang['You must be connected to read help user'] = 'Du må være koblet til for å lese hjelp bruker'; 
+$lang['You mustn\'t be connected to read help guest'] = 'Du må ikke være koblet til for å lese hjelp gjest';
+
+$lang['Language'] = 'Språk';
+$lang['Authors'] = 'Forfattere';
+$lang['Manage authors'] = 'Behandle forfattere'; 
+$lang['Add an author'] = 'Legg til en forfatter'; 
+$lang['Are you sure you want to remove selected authors?'] = 'Er du sikker på at du vil fjerne valgte forfattere?'; 
+$lang['Delete selected users'] = 'Slett valgte brukere'; 
+$lang['You must select at least one author.'] = 'Du må velge minst en forfatter.'; 
+$lang['This user does not exist in database.'] = 'Denne brukeren eksisterer ikke i databasen.'; 
+$lang['Added by']  = 'Lagt til av'; 
+$lang['Other extensions'] = 'Andre utvidelser';
+
+$lang['SVN configuration'] = 'SVN konfigurasjon'; 
+$lang['Repository URL'] = 'Oppbevarings URL'; 
+$lang['Archive root directory'] = 'Arkivets root mappe'; 
+$lang['Archive name'] = 'Arkiv navn'; 
+$lang['% will be replaced by version number'] = '% vil erstatte av versjons nummer'; 
+$lang['Delete SVN data'] = 'Slett SVN data'; 
+$lang['Are you sure you want to remove SVN data?'] = 'Er du sikker på at du vil fjerne SVN data?'; 
+$lang['SVN informations'] = 'SVN informasjon'; 
+$lang['Unable to retrieve SVN data!'] = 'Ikke i stand til å hente SVN data!'; 
+$lang['Characters not allowed in archive root directory.'] = 'Karakterer ikke lovlig i arkiv root mappe.'; 
+$lang['Characters not allowed in archive name.'] = 'Karakterer ikke lovlig i arkiv navn.'; 
+$lang['Upload a file'] = 'Last opp fil'; 
+$lang['Use SVN file creation'] = 'Bruk SVN fil lager'; 
+$lang['SVN revision'] = 'SVN revisjon'; 
+$lang['An error occured during SVN export.'] = 'En feil oppstod under SVN eksporteringen.';
+
+$lang['Available languages'] = 'Tilgjengelige språk'; 
+$lang['Default description'] = 'Standard beskrivelse'; 
+$lang['Default description can not be empty'] = 'Standard beskrivelse kan ikke være tom'; 
+$lang['extension'] = 'utvidelse'; 
+$lang['no extension'] = 'ingen utvidelse';
+
+$lang['Download last revision'] = 'Last ned siste revisjon'; 
+$lang['Download'] = 'Last ned';
+?>
