d36767d44f12439fe9d40427577e51db86502b22
ddtddt <ddtddt@piwigo.org>
2011-03-03 16:23:24 +0000

    Add tr_TR thanks to nakre
    git-svn-id: http://piwigo.org/pem_svn/trunk@267 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/tr_TR/common.lang.php
@@ -0,0 +1,170 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'Hakkında';
+$lang['Add/Modify an extension'] = 'Bir Eklenti Düzenle/Ekle';
+$lang['Add a link'] = 'Bağlantı Ekle';
+$lang['Add a revision'] = 'Bir değişiklik ekle';
+$lang['Add an extension'] = 'Bir eklenti ekle';
+$lang['Add or replace screenshot'] = 'Ekran görüntüsü ekle/yenile';
+$lang['Administration'] = 'Yönetim';
+$lang['Agreement'] = 'Anlaşma';
+$lang['agreement_description'] = 'Katkılarıma ait fikri mülkiyetini alan şartları ve kullanım koşullarını kabul ederim.';
+$lang['Are you sure you want to delete this item?'] = 'Bu öğeyi silmek istediğinizden emin misiniz?';
+$lang['Author'] = 'Yapımcı';
+$lang['Automatic redirection in %s seconds.'] = 'Otomatik yönlendirme %s saniye içinde.';
+$lang['Back to previous page'] = 'Önceki sayfaya dön';
+$lang['Can only resize PNG and JPEG files'] = 'Sadece PNG ve JPEG dosyaları yeniden boyutlandırabilir';
+$lang['CAPTCHA_error'] = 'Yazdığınız kelimeler uyuşmuyor.Lütfen tekrar deneyiniz.';
+$lang['Categories'] = 'Kategoriler';
+$lang['Category'] = 'Kategori';
+$lang['Changes'] = 'Değişiklikler';
+$lang['Click here if don\'t want to wait.'] = 'Beklemek istemiyorsanız buraya tıklayınız.';
+$lang['Confirmation password does not match.'] = 'Onaylama şifresi uyuşmuyor.';
+$lang['Confirm password'] = 'Şifre onayla';
+$lang['compact view'] = 'Küçük görünüm';
+$lang['Compatibility'] = 'Uyumluluk';
+$lang['Compatible with'] = 'Uyumlu oldukları';
+$lang['Connection settings'] = 'Bağlantı ayarları';
+$lang['Deletion forbidden'] = 'Yasaklıyı Sil';
+$lang['Delete extension'] = 'Eklentiyi Sil';
+$lang['Delete link'] = 'Bağlantıyı Sil';
+$lang['Delete revision'] = 'Değişikliği sil';
+$lang['Delete screenshot'] = 'Ekran görüntüsünü sil';
+$lang['Description'] = 'Açıklama';
+$lang['Disclaimer'] = 'Feragatname';
+$lang['Disconnect'] = 'Çıkış';
+$lang['Download revision'] = 'Değişikliği indir';
+$lang['Downloads'] = 'İndirme Sayısı';
+$lang['Error'] = 'Hata';
+$lang['expand/collapse all'] = 'genişlet/daralt hepsini';
+$lang['extensions'] = 'eklentiler';
+$lang['Extension informations'] = 'Eklenti Bilgileri';
+$lang['Extension successfuly added. Thank you.'] = 'Eklenti başarıyla eklendi teşekkürler.';
+$lang['Extension successfuly deleted.'] = 'Eklenti başarıyla silindi.';
+$lang['File'] = 'Dosya';
+$lang['File too big. Filesize must not exceed %s.'] = 'Dosya çok büyük. Dosya boyutu %s aşmamalıdır.';
+$lang['Filter'] = 'Filtre';
+$lang['first'] = 'İlk';
+$lang['First revision date'] = 'First revision date';
+$lang['Hello %s'] = 'Merhaba %s';
+$lang['Home'] = 'Kişisel';
+$lang['Identification successful'] = 'Giriş başarılı';
+$lang['Incorrect extension identifier'] = 'Eklenti tanımlayıcı yanlış';
+$lang['Incorrect revision identifier'] = 'Değişiklik tanımlayıcı yanlış';
+$lang['Incorrect URL'] = 'Hatalı URL';
+$lang['Incorrect username'] = 'Hatalı kullanıcı adı';
+$lang['Incorrect username/password'] = 'Hatalı kullanıcı adı/şifre';
+$lang['Index'] = 'İndeks';
+$lang['last'] = 'Son';
+$lang['Latest revision date'] = 'En son değişiklik tarihi';
+$lang['Link name must not be empty'] = 'Bağlantı adı boş olmamalı';
+$lang['Login'] = 'Giriş';
+$lang['Logout successful'] = 'Çıkış başarılı';
+$lang['Mail address'] = 'Posta adresi';
+$lang['Manage links'] = 'Bağlantıları yönetin';
+$lang['Manage screenshots'] = 'Ekran görüntülerini yönet';
+$lang['Modify extension'] = 'Eklentileri Düzenle';
+$lang['Modify revision'] = 'Değişiklikleri Düzenle';
+$lang['Most recent extensions'] = 'En son eklentiler';
+$lang['My extensions'] = 'Eklentilerim';
+$lang['My personnal homepage'] = 'Kişisel Sayfam';
+$lang['Name'] = 'Ad';
+$lang['news feed'] = 'haberler beslemesi';
+$lang['next'] = 'Sonraki';
+$lang['No account?'] = 'Hesap Yok?';
+$lang['No extension match your filter'] = 'Filtrenize uygun eklenti yok';
+$lang['No revision available for this extension.'] = 'Bu eklenti için değişiklik mevcut değil. Ya hiçbir değişiklik yok ya da filtrede uyguladığınız versiyon ile uyumlu bir değişiklik yok.';
+$lang['no revision yet'] = 'değişiklik yok';
+$lang['Notes'] = 'Notlar';
+$lang['Only *.zip files are allowed'] = 'Sadece *.zip dosyaları kabul edilir';
+$lang['page generated in %s'] = 'Sayfa %s saniyede oluşturuldu';
+$lang['Password'] = 'Şifre';
+$lang['Personnal informations'] = 'Kişisel bilgiler';
+$lang['powered by'] = '<a href="https://gna.org/projects/pem/">PEM</a> tarafından';
+$lang['prev'] = 'Önceki';
+$lang['Problem during upload'] = 'Yükleme sırasında sorun';
+$lang['Rank'] = 'Rütbe';
+$lang['Register'] = 'Kayıt';
+$lang['Related links'] = 'İlgili bağlantılar';
+$lang['Released on'] = 'Yapım tarihi';
+$lang['Reset'] = 'Sıfırla';
+$lang['Revision'] = 'Değişiklik';
+$lang['Revision list'] = 'Değişiklik listesi';
+$lang['Revision successfuly added. Thank you.'] = 'Değişiklik başarıyla eklendi teşekkürler.';
+$lang['Revision successfuly deleted.'] = 'Değişiklik başarıyla silindi.';
+$lang['Search'] = 'Arama';
+$lang['See extension'] = 'Eklentiyi gör';
+$lang['Some fields are missing'] = 'Bazı alanlar eksik';
+$lang['standard view'] = 'Standart görünüm';
+$lang['Submit'] = 'Gönder';
+$lang['Success'] = 'Başarı';
+$lang['undefined extension identifier'] = 'Eklenti tanımlayıcı tanımlanmamış';
+$lang['undefined revision identifier'] = 'Değişiklik tanımlayıcı tanımlanmamış';
+$lang['Username already in use'] = 'Kullanıcı adı zaten kullanımda';
+$lang['Unknown extension'] = 'Bilinmeyen eklenti';
+$lang['URL'] = 'URL';
+$lang['Username'] = 'Kullanıcı adı';
+$lang['Version'] = 'Versiyon';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'Ekran görüntüsü için sadece PNG ve JPEG dosyaları yükleyebilirsiniz.';
+$lang['You did not upload anything!'] = 'Birşey yüklemediniz!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'Bir eklenti düzenlemek, silmek veya eklemek için giriş yapmalısınız.';
+$lang['You must be connected to reach this page.'] = 'Bu sayfaya ulaşmak için giriş yapmalısınız.';
+$lang['You must be the extension author to modify it.'] = 'Düzenlemek için eklenti yapımcısı olmanız gerekiyor.';
+$lang['help'] = 'Yardım';
+$lang['You must be connected to read help user'] = 'Kullanıcı yardım sayfasını okumak için giriş yapmalısınız';
+$lang['You mustn\'t be connected to read help guest'] = 'Misafir yardım sayfasını okumak için giriş yapmamalısınız';
+
+$lang['Language'] = 'Dil';
+$lang['Authors'] = 'Yapımcılar';
+$lang['Manage authors'] = 'Yapımcıları Yönet';
+$lang['Add an author'] = 'Yapımcı Ekle';
+$lang['Are you sure you want to remove selected authors?'] = 'Seçilen yapımcıları silmek istediğinizden emin misiniz?';
+$lang['Delete selected users'] = 'Seçilen kullanıcıları sil';
+$lang['You must select at least one author.'] = 'En az bir yapımcı seçmelisiniz.';
+$lang['This user does not exist in database.'] = 'Bu kullanıcı veritabanında yok.';
+$lang['Added by']  = 'Ekleyen';
+$lang['Other extensions'] = 'Diğer eklentiler';
+
+$lang['SVN configuration'] = 'SVN yapılandırma';
+$lang['Repository URL'] = 'Depo URL\'si';
+$lang['Archive root directory'] = 'Arşiv kök dizini';
+$lang['Archive name'] = 'Arşiv adı';
+$lang['% will be replaced by version number'] = '% versiyonu ile değiştirilecektir';
+$lang['Delete SVN data'] = 'SVN verilerini sil';
+$lang['Are you sure you want to remove SVN data?'] = 'SVN verilerini silmek istediğinizden emin misiniz?';
+$lang['SVN informations'] = 'SVN verileri';
+$lang['Unable to retrieve SVN data!'] = 'SVN verileri geri alınamadı!';
+$lang['Characters not allowed in archive root directory.'] = 'Karakterlere arşiv kök dizin adında izin verilmiyor.';
+$lang['Characters not allowed in archive name.'] = 'Karakterlere arşiv adında izin verilmiyor.';
+$lang['Upload a file'] = 'Bir dosya yükle';
+$lang['Use SVN file creation'] = 'SVN dosya oluşturmayı kullan';
+$lang['SVN revision'] = 'SVN değişiklik';
+$lang['An error occured during SVN export.'] = 'SVN dışa aktarma sırasında hata oluştu.';
+
+$lang['Available languages'] = 'Mevcut diller';
+$lang['Default description'] = 'Varsayılan açıklama';
+$lang['Default description can not be empty'] = 'Varsayılan açıklama boş olamaz';
+$lang['extension'] = 'eklenti';
+$lang['no extension'] = 'eklenti yok';
+
+$lang['Download last revision'] = 'Son değişiklikleri indir';
+$lang['Download'] = 'İndir';
+?>
