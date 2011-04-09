b7716b53571b8854affd540430d5aa11b44c97e4
ddtddt <ddtddt@piwigo.org>
2011-04-09 22:03:51 +0000

    Add ar_SA thanks to bakir
    git-svn-id: http://piwigo.org/pem_svn/trunk@269 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/ar_SA/common.lang.php
@@ -0,0 +1,170 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'عن الموقع';
+$lang['Add/Modify an extension'] = 'اضافة او ازالة ملحق';
+$lang['Add a link'] = 'اضافة رابط';
+$lang['Add a revision'] = 'اضافة تغيير';
+$lang['Add an extension'] = 'اضافة ملحق';
+$lang['Add or replace screenshot'] = 'اضافة او تغيير لقطة';
+$lang['Administration'] = 'الادارة';
+$lang['Agreement'] = 'الاتفاقية';
+$lang['agreement_description'] = 'أقبل هذه الشروط والظروف التي تتخذ اتفاق ملكيتي الفكرية على مساهمتي.';
+$lang['Are you sure you want to delete this item?'] = 'هل انت متأكد من حذف هذا ؟';
+$lang['Author'] = 'المؤلف';
+$lang['Automatic redirection in %s seconds.'] = 'التحول التلقائي في %s ثانية';
+$lang['Back to previous page'] = 'الرجوع للصفحة السابقة';
+$lang['Can only resize PNG and JPEG files'] = 'يستطيع فقط تحجيم PNG , JPEG ملفات';
+$lang['CAPTCHA_error'] = 'الكلمة المدخلة للا تتطابق حاول ثانية';
+$lang['Categories'] = 'التصنيفات';
+$lang['Category'] = 'التصنيف';
+$lang['Changes'] = 'التغييرات';
+$lang['Click here if don\'t want to wait.'] = 'اضغط هنا اذا كنت لاتريد الانتظار';
+$lang['Confirmation password does not match.'] = 'تأكيد كلمة المرور لا يتطابق';
+$lang['Confirm password'] = 'تأكيد كلمة المرور';
+$lang['compact view'] = 'العرض المضغوط';
+$lang['Compatibility'] = 'التوافق';
+$lang['Compatible with'] = 'متوافق مع';
+$lang['Connection settings'] = 'اعدادات الاتصال';
+$lang['Deletion forbidden'] = 'الحذف ممنوع';
+$lang['Delete extension'] = 'حذف الملحق';
+$lang['Delete link'] = 'حذف الرابط';
+$lang['Delete revision'] = 'حذف التغيير';
+$lang['Delete screenshot'] = 'حذف لقطة الشاشة';
+$lang['Description'] = 'الوصف';
+$lang['Disclaimer'] = 'التنويه';
+$lang['Disconnect'] = 'قطع الاتصال';
+$lang['Download revision'] = 'تحميل التعديل';
+$lang['Downloads'] = 'التحميلات';
+$lang['Error'] = 'خطأ';
+$lang['expand/collapse all'] = 'توسيع او تضيق الكل';
+$lang['extensions'] = 'الملحقات';
+$lang['Extension informations'] = 'معلومات الملحق';
+$lang['Extension successfuly added. Thank you.'] = 'الملحق تم اضافته بنجاح.شكرا لك';
+$lang['Extension successfuly deleted.'] = 'تم حذف الملحق';
+$lang['File'] = 'ملف';
+$lang['File too big. Filesize must not exceed %s.'] = 'الملف كبير. حجم الملف لا يجب ان يتجاوز %s';
+$lang['Filter'] = 'المرشح';
+$lang['first'] = 'الاول';
+$lang['First revision date'] = 'تاريخ او تعديل';
+$lang['Hello %s'] = 'مرحبا %s';
+$lang['Home'] = 'الصفحة الاولى';
+$lang['Identification successful'] = 'نجاح التحقق';
+$lang['Incorrect extension identifier'] = 'معرف الملحق خاطئ';
+$lang['Incorrect revision identifier'] = 'معرف التعديل خاطئ';
+$lang['Incorrect URL'] = 'رابط خاطئ';
+$lang['Incorrect username'] = 'اسم المستخدم خاطئ';
+$lang['Incorrect username/password'] = 'اسم المستخدم او كلمة المرور خاطئ';
+$lang['Index'] = 'فهرس';
+$lang['last'] = 'الاخير';
+$lang['Latest revision date'] = 'تاريخ اخر تعديل';
+$lang['Link name must not be empty'] = 'اسم الرابط يجب الا يكون فارغ';
+$lang['Login'] = 'الدخول';
+$lang['Logout successful'] = 'تسجيل الخروج نجح';
+$lang['Mail address'] = 'عنوان البريد';
+$lang['Manage links'] = 'تنظيم الروابط';
+$lang['Manage screenshots'] = 'تنظيم اللقطات المصورة';
+$lang['Modify extension'] = 'تعديل ملحق';
+$lang['Modify revision'] = 'تعديل الريفسين';
+$lang['Most recent extensions'] = 'اخر الملحقات';
+$lang['My extensions'] = 'ملحقاتي';
+$lang['My personnal homepage'] = 'صفحتي الشخصية';
+$lang['Name'] = 'الاسم';
+$lang['news feed'] = 'الاخبار';
+$lang['next'] = 'اللاحق';
+$lang['No account?'] = 'لا حساب؟';
+$lang['No extension match your filter'] = 'لا ملحق يتطابق مع مرشحك';
+$lang['No revision available for this extension.'] = 'لا ريفيشين متاح لهذا ا الملحق يمكن انه لا يوجد ريفشين على الاطلاق او الريفيشين لا يطابق اصادر المرشح';
+$lang['no revision yet'] = 'لا ريفيشن بعد';
+$lang['Notes'] = 'ملاحظات';
+$lang['Only *.zip files are allowed'] = 'ققظ *.zip ي الملفات المسموحة';
+$lang['page generated in %s'] = 'تم انشاء الصفحة في %s';
+$lang['Password'] = 'كلمة المرور';
+$lang['Personnal informations'] = 'المعلومات الشخصية';
+$lang['powered by'] = 'مشغلة من قبل<a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'السابق';
+$lang['Problem during upload'] = 'مشكلة اثناء الرفع';
+$lang['Rank'] = 'ترتيب';
+$lang['Register'] = 'التسجيل';
+$lang['Related links'] = 'الروابط ذات علاقة';
+$lang['Released on'] = 'اتيح في';
+$lang['Reset'] = 'اعادة تعيين';
+$lang['Revision'] = 'ريفيشن';
+$lang['Revision list'] = 'قائمة الريفيشن';
+$lang['Revision successfuly added. Thank you.'] = 'تم اضافة الريفيشن بنجاح.شكرا لك';
+$lang['Revision successfuly deleted.'] = 'تم حذف الريفيشن';
+$lang['Search'] = 'البحث';
+$lang['See extension'] = 'رؤية الملحقات';
+$lang['Some fields are missing'] = 'بعض الحقول مفقودة';
+$lang['standard view'] = 'العرض العادي';
+$lang['Submit'] = 'ارسل';
+$lang['Success'] = 'نجاح';
+$lang['undefined extension identifier'] = 'معرف الملحق غير معرف';
+$lang['undefined revision identifier'] = 'معرف الريفيشن غير معرف';
+$lang['Username already in use'] = 'اسم المستخدم قيد الاستخدام';
+$lang['Unknown extension'] = 'ملحق غير معروف';
+$lang['URL'] = 'رابط';
+$lang['Username'] = 'اسم المستخدم';
+$lang['Version'] = 'الاصدار';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'تستطيع رفع صور PNG , JPEG كلقطات مصورة';
+$lang['You did not upload anything!'] = 'لم تقم برفع اي شيء!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'يجب ان تكون متصلا ل اضافة , تعديل , حذف اي ملحق';
+$lang['You must be connected to reach this page.'] = 'يجب ان تكون متصلا للوصول لهذه الصفحة';
+$lang['You must be the extension author to modify it.'] = 'يجب ان تكون مؤلف الملحق لتستطيع تعديله';
+$lang['help'] = 'مساعدة';
+$lang['You must be connected to read help user'] = 'يجب ان تكون متصلا لقراءة مساعدة المستخدمون';
+$lang['You mustn\'t be connected to read help guest'] = 'يجب الا تون متصلا لقراءة مساعدة الضيوف';
+
+$lang['Language'] = 'اللغة';
+$lang['Authors'] = 'المؤلفون';
+$lang['Manage authors'] = 'تنظيم المؤلفون';
+$lang['Add an author'] = 'اضافة مؤلف';
+$lang['Are you sure you want to remove selected authors?'] = 'هل انت متأكد من حذف المؤلفون المختارون؟';
+$lang['Delete selected users'] = 'حذف المستخدمون المختارون';
+$lang['You must select at least one author.'] = 'على الاقل يجب ان تختار مؤلف واحد';
+$lang['This user does not exist in database.'] = 'هذا المستخدم غير موجود في قواعد البيانات';
+$lang['Added by']  = 'اضافة من قبل';
+$lang['Other extensions'] = 'لملحقات الاخرى';
+
+$lang['SVN configuration'] = 'اعدادت SVN';
+$lang['Repository URL'] = 'رابط المستودع';
+$lang['Archive root directory'] = 'جذر مجلد الارشيف';
+$lang['Archive name'] = 'Aاسم الارشيف';
+$lang['% will be replaced by version number'] = '% سوف تستبدل برقم الريفيشن';
+$lang['Delete SVN data'] = 'حذف معلومات SVN';
+$lang['Are you sure you want to remove SVN data?'] = 'هل انت متاكد من انك تريد حذف معلومات الSVN';
+$lang['SVN informations'] = 'معلومات SVN';
+$lang['Unable to retrieve SVN data!'] = 'غير قادر على الحصول على SVN المعلومات';
+$lang['Characters not allowed in archive root directory.'] = 'الاحرف غير مسموحة في جذر مجلد الارشيف';
+$lang['Characters not allowed in archive name.'] = 'الاحرف غير مسموحة في اسم الارشيف';
+$lang['Upload a file'] = 'رفع ملف';
+$lang['Use SVN file creation'] = 'استخدم SVN لق الملف';
+$lang['SVN revision'] = 'SVN ريفيشن';
+$lang['An error occured during SVN export.'] = 'حدث خطأ اثناء التصيدير لل SVN';
+
+$lang['Available languages'] = 'اللغات المتاحة';
+$lang['Default description'] = 'الوصف الافتراضي';
+$lang['Default description can not be empty'] = 'الوصف الافتراضي لا يجب ان يكون فارغا';
+$lang['extension'] = 'ملحقات';
+$lang['no extension'] = 'لا ملحقات';
+
+$lang['Download last revision'] = 'تحميل اخر ريفيشن';
+$lang['Download'] = 'تحميل';
+?>
