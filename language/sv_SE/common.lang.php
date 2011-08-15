9ce729e6d67e2d1e17dfb96d13897bf56d25d50c
ddtddt <ddtddt@piwigo.org>
2011-08-15 20:21:40 +0000

    [language] add common sv_SE Thanks to styxsailor
    git-svn-id: http://piwigo.org/pem_svn/trunk@274 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/sv_SE/common.lang.php
@@ -0,0 +1,171 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2011 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'Om';
+$lang['Add/Modify an extension'] = 'Lägg till/anpassa ett tillägg';
+$lang['Add a link'] = 'Lägg till en länk';
+$lang['Add a revision'] = 'Lägg till en revision';
+$lang['Add an extension'] = 'Lägg till ett tillägg';
+$lang['Add or replace screenshot'] = 'Lägg till eller ersätt skärmdump';
+$lang['Administration'] = 'Administration';
+$lang['Agreement'] = 'Överenskommelse';
+$lang['agreement_description'] = 'Jag accepterar att denna villkorsöverenskommelse får mina rättigheter till mitt bidrag.';
+$lang['Are you sure you want to delete this item?'] = 'Är du säker på att du vill radera?';
+$lang['Author'] = 'Upphovsman';
+$lang['Automatic redirection in %s seconds.'] = 'Automatisk omdirigering om %s sekunder.';
+$lang['Back to previous page'] = 'Tillbaks till föregående sida';
+$lang['Can only resize PNG and JPEG files'] = 'Kan bara skala PNG och JPEG filer';
+$lang['CAPTCHA_error'] = 'Orden matchar inte. Vänligen försök igen.';
+$lang['Categories'] = 'Kategorier';
+$lang['Category'] = 'Kategori';
+$lang['Changes'] = 'Ändringar';
+$lang['Click here if don\'t want to wait.'] = 'Klicka här om du inte vill vänta.';
+$lang['Confirmation password does not match.'] = 'Bekräftelse av lösenord stämmer inte.';
+$lang['Confirm password'] = 'Bekräfta lösenord';
+$lang['compact view'] = 'kompakt vy';
+$lang['Compatibility'] = 'Kompabilitet';
+$lang['Compatible with'] = 'Kompatibel med';
+$lang['Connection settings'] = 'Anslutningsinställningar';
+$lang['Deletion forbidden'] = 'Radering ej tillåten';
+$lang['Delete extension'] = 'Radera tillägg';
+$lang['Delete link'] = 'Radera länk';
+$lang['Delete revision'] = 'Radera revision';
+$lang['Delete screenshot'] = 'Radera skärmdump';
+$lang['Description'] = 'Beskrivining';
+$lang['Disclaimer'] = 'Ansvarsfriskrivning';
+$lang['Disconnect'] = 'Koppla från';
+$lang['Download revision'] = 'Ladda ner revision';
+$lang['Downloads'] = 'Nedladdnigar';
+$lang['Error'] = 'Fel';
+$lang['expand/collapse all'] = 'expandera/kollapsa allt';
+$lang['extensions'] = 'tillägg';
+$lang['Extension informations'] = 'Tilläggsinformation';
+$lang['Extension successfuly added. Thank you.'] = 'Tillägg tillagt. Tack.';
+$lang['Extension successfuly deleted.'] = 'Tillägg raderat.';
+$lang['File'] = 'Fil';
+$lang['File too big. Filesize must not exceed %s.'] = 'Filen är för stor. Filstorleken får ej överskrida %s.';
+$lang['Filter'] = 'Filter';
+$lang['first'] = 'Först';
+$lang['First revision date'] = 'Första revisionsdatum';
+$lang['Hello %s'] = 'Hej %s';
+$lang['Home'] = 'Hem';
+$lang['Identification successful'] = 'Lyckad identifiering';
+$lang['Incorrect extension identifier'] = 'Felaktig tilläggsidentifiering';
+$lang['Incorrect revision identifier'] = 'Felaktig revisionsidentifiering';
+$lang['Incorrect URL'] = 'Felaktig URL';
+$lang['Incorrect username'] = 'Felaktigt användarnamn';
+$lang['Incorrect username/password'] = 'Felaktigt användarnamn/lösenord';
+$lang['Index'] = 'Index';
+$lang['last'] = 'Senast';
+$lang['Latest revision date'] = 'Senaste revisionsdatum';
+$lang['Link name must not be empty'] = 'Länknamn får inte vara tomt';
+$lang['Login'] = 'Inloggning';
+$lang['Logout successful'] = 'Utloggning lyckad';
+$lang['Mail address'] = 'E-postadress';
+$lang['Manage links'] = 'Hantera länkar';
+$lang['Manage screenshots'] = 'Hantera skärmdumpar';
+$lang['Modify extension'] = 'Hantera tillägg';
+$lang['Modify revision'] = 'Hantera revision';
+$lang['Most recent extensions'] = 'Senaste tillägg';
+$lang['My extensions'] = 'Mina tillägg';
+$lang['My personnal homepage'] = 'Min personliga hemsida';
+$lang['Name'] = 'Namn';
+$lang['news feed'] = 'nyhetsflöde';
+$lang['next'] = 'Nästa';
+$lang['No account?'] = 'Inget konto?';
+$lang['No extension match your filter'] = 'Inga tillägg matchar ditt filter';
+$lang['No revision available for this extension.'] = 'Ingen revision tillgänlig för detta tillägg. Antingen på grund av att det inte finns någon revision eller för att ingen revision kompatibel med ditt versionsfilter hittades.';
+$lang['no revision yet'] = 'ingen revsion ännu';
+$lang['Notes'] = 'Anteckningar';
+$lang['Only *.zip files are allowed'] = 'Bara *.zip filer är tillåtna';
+$lang['page generated in %s'] = 'sidan genererad i %s';
+$lang['Password'] = 'Lösenord';
+$lang['Personnal informations'] = 'Personlig information';
+$lang['powered by'] = 'driven av <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'Tidigare';
+$lang['Problem during upload'] = 'Problem under uppladdning';
+$lang['Rank'] = 'Rang';
+$lang['Register'] = 'Register';
+$lang['Related links'] = 'Relatede länkar';
+$lang['Released on'] = 'Utgiven';
+$lang['Reset'] = 'Återställ';
+$lang['Revision'] = 'Revision';
+$lang['Revision list'] = 'Revisionslista';
+$lang['Revision successfuly added. Thank you.'] = 'Revision adderad. Tack.';
+$lang['Revision successfuly deleted.'] = 'Revision raderad.';
+$lang['Search'] = 'Sök';
+$lang['See extension'] = 'Se tillägg';
+$lang['Some fields are missing'] = 'Det saknas fält';
+$lang['standard view'] = 'standard vy';
+$lang['Submit'] = 'Skicka';
+$lang['Success'] = 'Lyckad';
+$lang['undefined extension identifier'] = 'odefinierad tillägsidentifierare';
+$lang['undefined revision identifier'] = 'odefinierad revisionsidentifierare';
+$lang['Username already in use'] = 'Användarnamner används redan';
+$lang['Unknown extension'] = 'Okänt tillägg';
+$lang['URL'] = 'URL';
+$lang['Username'] = 'Användarnamn';
+$lang['Version'] = 'Version';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'du kan bara ladda upp PNG och JPEG filer som skärmdump.';
+$lang['You did not upload anything!'] = 'Du laddade inte upp något!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'Du måste vara ansluten för att lägga till, modifiera eller radera ett tillägg.';
+$lang['You must be connected to reach this page.'] = 'Du måste vara ansluten för att nå denna sida.';
+$lang['You must be the extension author to modify it.'] = 'Du måste vara tilläggets upphovsman för att ändra det.';
+$lang['help'] = 'Hjälp';
+$lang['You must be connected to read help user'] = 'Du måste vara ansluten för att läsa användarhjälpen';
+$lang['You mustn\'t be connected to read help guest'] = 'Du kan inte vara ansluten för att läsa besökarhjälpen';
+
+$lang['Language'] = 'Språk';
+$lang['Authors'] = 'Upphovsmän';
+$lang['Manage authors'] = 'Hantera upphovsmän';
+$lang['Add an author'] = 'Lägg till en upphovsman';
+$lang['Are you sure you want to remove selected authors?'] = 'Är du säker på att du vill ta bort valda upphovsmän?';
+$lang['Delete selected users'] = 'Radera valda användare';
+$lang['You must select at least one author.'] = 'Du måste ange minst en upphovsman.';
+$lang['This user does not exist in database.'] = 'Den här användaren finns inte i databasen.';
+$lang['Added by']  = 'Tillagd av';
+$lang['Other extensions'] = 'Andra tillägg';
+
+$lang['SVN configuration'] = 'SVN konfigurering';
+$lang['Repository URL'] = 'Repository URL';
+$lang['Archive root directory'] = 'Arkivets rotmapp';
+$lang['Archive name'] = 'Arkivnamn';
+$lang['% will be replaced by version number'] = '% kommer ersättas av  version nummer';
+$lang['Delete SVN data'] = 'Radera SVN data';
+$lang['Are you sure you want to remove SVN data?'] = 'Bekräfta radering av SVN data?';
+$lang['SVN informations'] = 'SVN information';
+$lang['Unable to retrieve SVN data!'] = 'Kan ej hämta SVN data!';
+$lang['Characters not allowed in archive root directory.'] = 'Tecken inte tillåtna i arkivets rotmapp.';
+$lang['Characters not allowed in archive name.'] = 'Tecken inte tillåtna i arkivnamn.';
+$lang['Upload a file'] = 'Ladda upp en fil';
+$lang['Use SVN file creation'] = 'Använd SVN för att skapa fil';
+$lang['SVN revision'] = 'SVN revision';
+$lang['An error occured during SVN export.'] = 'Ett fel uppstod under SVN export.';
+
+$lang['Available languages'] = 'Tillgängliga språk';
+$lang['Default description'] = 'Standardbeskrivning';
+$lang['Default description can not be empty'] = 'Standardbeskrivning kan inte vara tom';
+$lang['extension'] = 'tillägg';
+$lang['no extension'] = 'inget tillägg';
+
+$lang['Download last revision'] = 'Ladda ner senaste revision';
+$lang['Download'] = 'Ladda ner';
+$lang['releases'] = 'releaser';
+?>
