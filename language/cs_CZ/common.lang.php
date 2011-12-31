5093e55cef6c8be3f7e7eb80f0191dc48fc0c7ee
ddtddt <ddtddt@piwigo.org>
2011-12-31 10:07:55 +0000

    [language] add common cs_CZ Thanks to webprostor.eu
    git-svn-id: http://piwigo.org/pem_svn/trunk@279 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/cs_CZ/common.lang.php
@@ -0,0 +1,170 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'O nás';
+$lang['Add/Modify an extension'] = 'Pridat/Editovat rozšírení';
+$lang['Add a link'] = 'Pridat odkaz';
+$lang['Add a revision'] = 'Pridat revizi';
+$lang['Add an extension'] = 'Pridat rozšírení';
+$lang['Add or replace screenshot'] = 'Vložit nebo nahradit screenshot';
+$lang['Administration'] = 'Administrace';
+$lang['Agreement'] = 'Souhlas';
+$lang['agreement_description'] = 'Souhlasím s temito podmínkami, které se vztahují k mému intelektuálnímu vlastnictví mých vlastních rozšírení.';
+$lang['Are you sure you want to delete this item?'] = 'Opravdu chcete odstranit tuto položku?';
+$lang['Author'] = 'Autor';
+$lang['Automatic redirection in %s seconds.'] = 'Automatické presmerování za %s sekund.';
+$lang['Back to previous page'] = 'Zpet na predchozí stránku';
+$lang['Can only resize PNG and JPEG files'] = 'Velikost lze zmenit jen u PNG a JPEG';
+$lang['CAPTCHA_error'] = 'Zapsaná slova se neshodují. Prosím, zkuste to znova.';
+$lang['Categories'] = 'Kategorie';
+$lang['Category'] = 'Kategorie';
+$lang['Changes'] = 'Zmeny';
+$lang['Click here if don\'t want to wait.'] = 'Kliknete sem pokud nechcete cekat.';
+$lang['Confirmation password does not match.'] = 'Potvrzení, hesla se neshodují.';
+$lang['Confirm password'] = 'Overení hesla';
+$lang['compact view'] = 'kompaktní zobrazení';
+$lang['Compatibility'] = 'Kompatibilita';
+$lang['Compatible with'] = 'Kompatibilní s';
+$lang['Connection settings'] = 'Nastavení pripojení';
+$lang['Deletion forbidden'] = 'Odstranení zakázáno';
+$lang['Delete extension'] = 'Odstranit rozšírení';
+$lang['Delete link'] = 'Odstranit odkaz';
+$lang['Delete revision'] = 'Odstranit revizi';
+$lang['Delete screenshot'] = 'Odstranit screenshot';
+$lang['Description'] = 'Popis';
+$lang['Disclaimer'] = 'Disclaimer/Prohlášení';
+$lang['Disconnect'] = 'Odpojit';
+$lang['Download revision'] = 'Uložit revizi';
+$lang['Downloads'] = 'Soubory ke stažení';
+$lang['Error'] = 'Chyba';
+$lang['expand/collapse all'] = 'rozbalit/zabalit vše';
+$lang['extensions'] = 'rozšírení';
+$lang['Extension informations'] = 'Informace o rozšírení';
+$lang['Extension successfuly added. Thank you.'] = 'Rozšírení bylo pridáno. Dekujeme vám.';
+$lang['Extension successfuly deleted.'] = 'Rozšírení kompletne odstraneno.';
+$lang['File'] = 'Soubor';
+$lang['File too big. Filesize must not exceed %s.'] = 'Soubor je príliš velký. Velikost nesmí prekrocit %s.';
+$lang['Filter'] = 'Filtr';
+$lang['first'] = 'První';
+$lang['First revision date'] = 'Datum první revize';
+$lang['Hello %s'] = 'Ahoj %s';
+$lang['Home'] = 'Home';
+$lang['Identification successful'] = 'Identifikace úspešná';
+$lang['Incorrect extension identifier'] = 'Nesprávný identifikátor rozšírení';
+$lang['Incorrect revision identifier'] = 'Nesprávný identifikátor revize';
+$lang['Incorrect URL'] = 'Nesprávná URL';
+$lang['Incorrect username'] = 'Nesprávné uživatelské jméno';
+$lang['Incorrect username/password'] = 'Nesprávný název živatele/heslo';
+$lang['Index'] = 'Index';
+$lang['last'] = 'Poslední';
+$lang['Latest revision date'] = 'Poslední datum revize';
+$lang['Link name must not be empty'] = 'Název linku nemuže být prázdný';
+$lang['Login'] = 'Prihlášení';
+$lang['Logout successful'] = 'Odhlášení provedeno';
+$lang['Mail address'] = 'Mail adresa';
+$lang['Manage links'] = 'Spravovat odkazy';
+$lang['Manage screenshots'] = 'Spravovat náhledy';
+$lang['Modify extension'] = 'Upravit rozšírení';
+$lang['Modify revision'] = 'Upravit revizi';
+$lang['Most recent extensions'] = 'Nejžádanejší rozšírení';
+$lang['My extensions'] = 'Moje rozšírení';
+$lang['My personnal homepage'] = 'Moje osobní stránka';
+$lang['Name'] = 'Jméno';
+$lang['news feed'] = 'novinky';
+$lang['next'] = 'Další';
+$lang['No account?'] = 'Nemáte úcet?';
+$lang['No extension match your filter'] = 'Žádné rozšírení neodpovídá vašemu filtrování';
+$lang['No revision available for this extension.'] = 'Žádná revize pro toto rozšírení není k dispozici. Možná proto že proste není nebo protože není odopovídající revize pro váš nastavený filtr.';
+$lang['no revision yet'] = 'zatím bez revize';
+$lang['Notes'] = 'Poznámky';
+$lang['Only *.zip files are allowed'] = 'Pouze zip soubory jsou povoleny';
+$lang['page generated in %s'] = 'stránka vygenerována za %s';
+$lang['Password'] = 'Heslo';
+$lang['Personnal informations'] = 'Osobní údaje';
+$lang['powered by'] = 'beží díky <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'Predchozí';
+$lang['Problem during upload'] = 'Problém pri nahrávání';
+$lang['Rank'] = 'Poradí';
+$lang['Register'] = 'Zaregistrovat';
+$lang['Related links'] = 'Související odkazy';
+$lang['Released on'] = 'Vydáno';
+$lang['Reset'] = 'Reset';
+$lang['Revision'] = 'Revize';
+$lang['Revision list'] = 'Seznam revizí';
+$lang['Revision successfuly added. Thank you.'] = 'Revize byla vložena. Dekujeme vám.';
+$lang['Revision successfuly deleted.'] = 'Revize byla odstranena.';
+$lang['Search'] = 'Hledat';
+$lang['See extension'] = 'Ukázat doplnek';
+$lang['Some fields are missing'] = 'Nekterá pole nejsou vyplnena';
+$lang['standard view'] = 'standardní zobrazení';
+$lang['Submit'] = 'Odeslat';
+$lang['Success'] = 'Úspech';
+$lang['undefined extension identifier'] = 'nedefinován identifikátor doplnku';
+$lang['undefined revision identifier'] = 'nedefinován identifikátor revize';
+$lang['Username already in use'] = 'Uživatelské jméno je už používáno';
+$lang['Unknown extension'] = 'Neznámý doplnek';
+$lang['URL'] = 'URL';
+$lang['Username'] = 'Jméno uživatele';
+$lang['Version'] = 'Verze';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'Mužete nahrát jen PNG a JPEG soubory jako náhledy.';
+$lang['You did not upload anything!'] = 'Nelze uploadovat cokoliv!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'Musíte být prihlášen pro pridání, úpravu nebo odstranení doplnku.';
+$lang['You must be connected to reach this page.'] = 'Musíte být prihlášen pro prístup na tuto stránku.';
+$lang['You must be the extension author to modify it.'] = 'Musíte být autor doplnku aby ho bylo možné upravovat.';
+$lang['help'] = 'Nápoveda';
+$lang['You must be connected to read help user'] = 'Musíte být pripojen pro ctení nápovedy jako uživatel';
+$lang['You mustn\'t be connected to read help guest'] = 'Nemusíte být pripojen pro ctení nápovedy jako host';
+$lang['Language'] = 'Jazyk';
+$lang['Authors'] = 'Autor';
+$lang['Manage authors'] = 'Upravit autora';
+$lang['Add an author'] = 'Pridat autora';
+$lang['Are you sure you want to remove selected authors?'] = 'Opravdu chcete odstranit zvolené autory?';
+$lang['Delete selected users'] = 'Odstranit vybrané uživatele';
+$lang['You must select at least one author.'] = 'Musíte oznacit alespon jednoho autora.';
+$lang['This user does not exist in database.'] = 'Tento uživatel neexistuje v databázi.';
+$lang['Added by'] = 'Vložil';
+$lang['Other extensions'] = 'Ostatní rozšírení';
+$lang['SVN configuration'] = 'SVN konfigurace';
+$lang['Repository URL'] = 'URL Repozitáre';
+$lang['Archive root directory'] = 'Hlavní složka archivu';
+$lang['Archive name'] = 'Název archivu';
+$lang['% will be replaced by version number'] = '% bude nahrazeno císlem verze';
+$lang['Delete SVN data'] = 'Odstranit SVN data';
+$lang['Are you sure you want to remove SVN data?'] = 'Opravdu chcete odstranit SVN data?';
+$lang['SVN informations'] = 'SVN informace';
+$lang['Unable to retrieve SVN data!'] = 'Nelze prijmout SVN data!';
+$lang['Characters not allowed in archive root directory.'] = 'Nepovolené znaky v hlavní složce archivu.';
+$lang['Characters not allowed in archive name.'] = 'Nepovolené znaky v názvu archivu.';
+$lang['Upload a file'] = 'Uploadovat soubor';
+$lang['Use SVN file creation'] = 'Použít vytvárení SVN souboru';
+$lang['SVN revision'] = 'SVN revize';
+$lang['An error occured during SVN export.'] = 'Nastala chyba pri provádení SVN export.';
+$lang['Available languages'] = 'Preklady k dispozici';
+$lang['Default description'] = 'Výchozí popisek';
+$lang['Default description can not be empty'] = 'Výchozí popisek nemuže být prázdný';
+$lang['extension'] = 'doplnek';
+$lang['no extension'] = 'žádný doplnek';
+$lang['Download last revision'] = 'Download poslední revize';
+$lang['Download'] = 'Download';
+$lang['releases'] = 'vydání';
+$lang['Choose compatibility versions...'] = 'Vyberte kompatibilní verze...';
+$lang['Home'] = 'Home';
+$lang['Index'] = 'Index';
+?>
