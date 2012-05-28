6e1646f3c7d5a1a0618467c837a4618c8e9a3dff
ddtddt <ddtddt@piwigo.org>
2012-05-28 12:25:48 +0000

    [language] add ca_ES - Catalan - Català Thanks to AlbertParera
    git-svn-id: http://piwigo.org/pem_svn/trunk@305 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/ca_ES/common.lang.php
@@ -0,0 +1,194 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'Quant a';
+$lang['Add/Modify an extension'] = 'Afegir/Modificar extensió';
+$lang['Add a link'] = 'Afegir enllaç';
+$lang['Add a revision'] = 'Afegir revisió';
+$lang['Add or replace screenshot'] = 'Afegir o reemplaçar una captura de pantalla';
+$lang['Add an extension'] = 'Afegir extensió';
+$lang['Administration'] = 'Administració';
+$lang['Agreement'] = 'Acord';
+$lang['agreement_description'] = 'Accepto els termes del contracte estipulat contribuïnt amb la transferència de la meva propietat intel·lectual.';
+$lang['Are you sure you want to delete this item?'] = 'Està segur que vol eliminar aquest element?';
+$lang['Author'] = 'Autor';
+$lang['Automatic redirection in %s seconds.'] = 'Redirecció automàtica en %s segons.';
+$lang['Back to previous page'] = 'Tornar a la pàgina anterior';
+$lang['Can only resize PNG and JPEG files'] = 'Només es poden redimensionar els arxius amb extensions PNG i JPEG';
+$lang['CAPTCHA_error'] = 'Les paraules introduïdes no coincideixen. Si us plau, torni-ho a intentar.';
+$lang['Categories'] = 'Categories';
+$lang['Category'] = 'Categoria';
+$lang['Changes'] = 'Canvis';
+$lang['Click here if don\'t want to wait.'] = 'Faci clic aquí so no vol esperar.';
+$lang['Confirmation password does not match.'] = 'La confirmació de la clau d\'accés no coincideix.';
+$lang['Confirm password'] = 'Confirmar clau d\'accés';
+$lang['compact view'] = 'Vista compacta';
+$lang['Compatibility'] = 'Compatibilitat';
+$lang['Compatible with'] = 'Compatible amb';
+$lang['Connection settings'] = 'Paràmetres de connexió';
+$lang['Deletion forbidden'] = 'Eliminació prohibida';
+$lang['Delete extension'] = 'Eliminar extensió';
+$lang['Delete link'] = 'Eliminar enllaç';
+$lang['Delete revision'] = 'Eliminar revisió';
+$lang['Delete screenshot'] = 'Eliminar captura de pantalla';
+$lang['Description'] = 'Descripció';
+$lang['Disclaimer'] = 'Renúncia';
+$lang['Disconnect'] = 'Desconnectar';
+$lang['Download revision'] = 'Descarregar revisió';
+$lang['Downloads'] = 'Descàrregues';
+$lang['expand/collapse all'] = 'Expandir/Contraure tot';
+$lang['extensions'] = 'extensions';
+$lang['Extension informations'] = 'Informació de l\'extensió';
+$lang['Extension successfuly added. Thank you.'] = 'L\'extensió s\ha afegit correctament. Gràcies.';
+$lang['Extension successfuly deleted.'] = 'L\'extensió s\'ha eliminat correctament.';
+$lang['Error'] = 'Error';
+$lang['File'] = 'Arxiu';
+$lang['File too big. Filesize must not exceed %s.'] = 'Arxiu massa gran. La mida dels arxius no pot excedir %s.';
+$lang['Filter'] = 'Filtrar';
+$lang['first'] = 'Primer';
+$lang['First revision date'] = 'Data de la primera revisió';
+$lang['Hello %s'] = 'Hola %s';
+$lang['Home'] = 'Inici';
+$lang['Identification successful'] = 'Identificació correcta';
+$lang['Incorrect extension identifier'] = 'Identificador d\'extensió incorrecte';
+$lang['Incorrect revision identifier'] = 'Identificador de revisió incorrecte';
+$lang['Incorrect URL'] = 'URL incorrecta';
+$lang['Incorrect username'] = 'Nom d\'usuari incorrecte';
+$lang['Incorrect username/password'] = 'Nom d\'usuari o clau d\'accés incorrecte';
+$lang['Index'] = 'Índex';
+$lang['last'] = 'Última';
+$lang['Latest revision date'] = 'Data de l\'última revisió';
+$lang['Link name must not be empty'] = 'El nom de l\'enllaç no pot estar buit';
+$lang['Login'] = 'Autenticació';
+$lang['Logout successful'] = 'Sessió finalitzada correctament';
+$lang['Mail address'] = 'Correu electrònic';
+$lang['Manage links'] = 'Gestionar enllaços';
+$lang['Manage screenshots'] = 'Gestionar captures de pantalla';
+$lang['Modify extension'] = 'Modifiar extensió';
+$lang['Modify revision'] = 'Modificar revisió';
+$lang['Most recent extensions'] = 'Extensions més recents';
+$lang['My extensions'] = 'Les meves txtensions';
+$lang['My personnal homepage'] = 'Pàgina personal';
+$lang['Name'] = 'Nom';
+$lang['news feed'] = 'canal RSS';
+$lang['next'] = 'Següent';
+$lang['No account?'] = 'No disposa de cap compte?';
+$lang['No extension match your filter'] = 'No hi han extensions que coincideixin amb el seu filtre';
+$lang['No revision available for this extension.'] = 'No hi ha revisions disponibles per a aquesta extensió. Potser no hi ha revisió per a aquesta extensió, o cap revisió coincideixi amb el seu filtre'.
+$lang['no revision yet'] = 'Encara no hi ha cap revisió';
+$lang['Notes'] = 'Notes';
+$lang['Only *.zip files are allowed'] = 'Només són permesos els arxius *.zip';
+$lang['page generated in %s'] = 'Pàgina generada en %s';
+$lang['Password'] = 'Clau d\'accés';
+$lang['Personnal informations'] = 'Informació personal';
+$lang['powered by'] = 'Desenvolupat per <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'Anterior';
+$lang['Problem during upload'] = 'S\'ha produït un problema durant la càrrega';
+$lang['Rank'] = 'Rang';
+$lang['Register'] = 'Inscriure\'s';
+$lang['Related links'] = 'Enllaços';
+$lang['Released on'] = 'Afegit el';
+$lang['Reset'] = 'Restablir';
+$lang['Revision'] = 'Revisió';
+$lang['Revision list'] = 'Llista de revisions';
+$lang['Revision successfuly added. Thank you.'] = 'La revisió s\'ha afegit correctament. Gràcies.';
+$lang['Revision successfuly deleted.'] = 'La revisió s\'ha eliminat correctament.';
+$lang['Search'] = 'Cercar';
+$lang['See extension'] = 'Veure extensió';
+$lang['Some fields are missing'] = 'Falten alguns camps';
+$lang['standard view'] = 'Vista estàndard';
+$lang['Submit'] = 'Validar';
+$lang['Success'] = 'Acció realitzada correctament';
+$lang['undefined extension identifier'] = 'Identificador d\'extensió indefinit';
+$lang['undefined revision identifier'] = 'Identificador de revisió indefinit';
+$lang['Username already in use'] = 'El nom d\'usuari està en ús';
+$lang['Unknown extension'] = 'Extensió desconeguda';
+$lang['URL'] = 'URL';
+$lang['Username'] = 'Nom d\'usuari';
+$lang['Version'] = 'Versió';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'Només es poden carregar arxius amb extensions PNG i JPEG.';
+$lang['You did not upload anything!'] = 'No ha carregat cap arxiu!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'Ha d\'estar connectat per afegir, modificar o eliminar una extensió.';
+$lang['You must be connected to reach this page.'] = 'Ha d\'estar connectat per accedir a aquesta pàgina.';
+$lang['You must be the extension author to modify it.'] = 'Només l\'autor de l\'extensió pot realitzar modificacions.';
+$lang['help'] = 'Ajuda';
+$lang['You must be connected to read help user'] = 'Ha d\'estar connectat per llegir l\'ajuda a l\'usuari.';
+$lang['You mustn\'t be connected to read help guest'] = 'No cal que estigui connectat per llegir l\'ajuda als convidats (usuaris no inscrits).';
+$lang['Language'] = 'Idioma';
+$lang['Authors'] = 'Autors';
+$lang['Manage authors'] = 'Gestionar autors';
+$lang['Add an author'] = 'Afegir autor';
+$lang['Are you sure you want to remove selected authors?'] = 'Està segur que vol eliminar els usuaris seleccionats?';
+$lang['Delete selected users'] = 'Eliminar els usuaris seleccionats';
+$lang['You must select at least one author.'] = 'Cal seleccionar almenys un autor.';
+$lang['This user does not exist in database.'] = 'Aquest usuari no existeix a la base de dades.';
+$lang['Added by']  = 'Afegit per';
+$lang['Other extensions'] = 'Altres extensions';
+$lang['SVN configuration'] = 'Configuració SVN';
+$lang['Repository URL'] = 'URL del repositori';
+$lang['Archive root directory'] = 'Directori arrel de l\'arxiu';
+$lang['Archive name'] = 'Nom d\'arxiu';
+$lang['% will be replaced by version number'] = '% serà reemplaçat pel número de versió';
+$lang['Delete SVN data'] = 'Eliminar informació SVN';
+$lang['Are you sure you want to remove SVN data?'] = 'Està segur que vol eliminar la informació SVN?';
+$lang['SVN informations'] = 'Informació SVN';
+$lang['Unable to retrieve SVN data!'] = 'No ha sigut possible recuperar la informació SVN!';
+$lang['Characters not allowed in archive root directory.'] = 'Caràcters no permesos en el directori arrel de l\'arxiu.';
+$lang['Characters not allowed in archive name.'] = 'Caràcters no permesos en el nom de l\'arxiu';
+$lang['Upload a file'] = 'Carregar un arxiu';
+$lang['Use SVN file creation'] = 'Crear un arxiu per SVN';
+$lang['SVN revision'] = 'Revisió SVN';
+$lang['An error occured during SVN export.'] = 'Hi ha hagut un error durant l\'exportació SVN.';
+$lang['Available languages'] = 'Idiomes disponibles';
+$lang['Default description'] = 'Descripció predeterminada';
+$lang['Default description can not be empty'] = 'Cal introduïr la descripció predeterminada';
+$lang['extension'] = 'extensió';
+$lang['no extension'] = 'No hi ha cap extensió';
+$lang['Download last revision'] = 'Descarregar l\'última revisió';
+$lang['Download'] = 'Descarregar';
+$lang['releases'] = 'versions';
+$lang['Choose compatibility versions...'] = 'Escull les versions compatibles...';
+$lang['Tags'] = 'Etiquetes';
+$lang['Type in a search term'] = 'Introdueix un criteri de cerca';
+$lang['No results'] = 'No hi ha resultats';
+$lang['Searching...'] = 'cercant...';
+$lang['new'] = 'nou';
+$lang['Rated %d times'] = 'Valorat %d vegades';
+$lang['Average rating'] = 'Valoració promig';
+$lang['not rated yet'] = 'encara no ha sigut valorat';
+$lang['cancel this rating!'] = 'cancel·lar aquesta valoració!';
+$lang['Update your rating'] = 'Canviar de valoració';
+$lang['Rate it!'] = 'Valorar!';
+$lang['Your rating'] = 'La seva valoració';
+$lang['All'] = 'Totes';
+$lang['Thank you!'] = 'Gràcies!';
+$lang['Thank you! Your review is awaiting moderation.'] = 'Gràcies! El seu comentari està pendent de validació.';
+$lang['Please enter your name'] = 'Si us plau, introdueixi el seu nom';
+$lang['Please enter your email'] = 'Si us plau, introdueixi el seu correu electrònic';
+$lang['The review is empty!'] = 'Sense opinions!';
+$lang['Please rate this extension'] = 'Si us plau, valori aquesta extensió';
+$lang['Reviews'] = 'Opinions';
+$lang['Add a review'] = 'Afegir una opinió';
+$lang['Email (not displayed)'] = 'Correu electrònic (no visible)';
+$lang['Review summary'] = 'Resum';
+$lang['Your review'] = 'Opinar';
+$lang['Send'] = 'Enviar';
+$lang['Please do not use this form to request assistance or report a bug. Use the forums instead.'] = 'Si us plau, no utilitzi aquest formulari per a demanar ajuda o per informar d\'errors. En tot cas faci-ho per mitjà del fòrum.';
+?>
