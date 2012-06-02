bcd7527ad28c86a7882fb909c100f0fe01fbf1ee
ddtddt <ddtddt@piwigo.org>
2012-06-02 07:19:47 +0000

    [language] update el_GR- Greek - ελληνικά Thanks to bas_alba
    git-svn-id: http://piwigo.org/pem_svn/trunk@308 d4d6b01b-5e10-0410-8d5c-99ab1f9c6602

---
+++ language/el_GR/common.lang.php
@@ -0,0 +1,202 @@
+﻿<?php
+// +-----------------------------------------------------------------------+
+// | PEM - a PHP based Extension Manager                                   |
+// | Copyright (C) 2005-2009 PEM Team - http://home.gna.org/pem            |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['About'] = 'Σχετικά';
+$lang['Add/Modify an extension'] = 'Προσθήκη / Τροποποίηση επέκτασης';
+$lang['Add a link'] = 'Προσθήκη συνδέσμου';
+$lang['Add a revision'] = 'Προσθέστε μια αναθεώρηση';
+$lang['Add an extension'] = 'Προσθέστε μια επέκταση';
+$lang['Add or replace screenshot'] = 'Προσθήκη ή αντικατάσταση στιγμιότυπου οθόνης';
+$lang['Administration'] = 'Διαχείριση';
+$lang['Agreement'] = 'Συμφωνία';
+$lang['agreement_description'] = 'Αποδέχομαι τους όρους  και τις προϋποθέσεις αυτής τη συμφωνίας που λαμβάνει την πνευματική μου ιδιοκτησία με τη συμβολή μου.';
+$lang['Are you sure you want to delete this item?'] = 'Είστε βέβαιοι ότι θέλετε να διαγράψετε αυτό το στοιχείο;';
+$lang['Author'] = 'Δημιουργός';
+$lang['Automatic redirection in %s seconds.'] = 'Αυτόματη ανακατεύθυνση σε %s δευτερόλεπτα.';
+$lang['Back to previous page'] = 'Επιστρέψτε στην προηγούμενη σελίδα';
+$lang['Can only resize PNG and JPEG files'] = 'Μπορεί να αλλάξετε το μέγεθος μόνο σε PNG και JPEG αρχεία';
+$lang['CAPTCHA_error'] = 'Οι 2 λέξεις που έχουν πληκτρολογηθεί δεν συμφωνούν. Παρακαλώ, προσπαθήστε ξανά.';
+$lang['Categories'] = 'Κατηγορίες';
+$lang['Category'] = 'Κατηγορία';
+$lang['Changes'] = 'Μεταβολές';
+$lang['Click here if don\'t want to wait.'] = 'Πατήστε εδώ αν δεν θέλετε να περιμένετε.';
+$lang['Confirmation password does not match.'] = 'Κωδικός επιβεβαίωσης δεν ταιριάζει.';
+$lang['Confirm password'] = 'Επιβεβαίωση κωδικού πρόσβασης';
+$lang['compact view'] = 'συμπαγής προβολή';
+$lang['Compatibility'] = 'Συμβατότητα';
+$lang['Compatible with'] = 'Συμβατότητα με';
+$lang['Connection settings'] = 'Ρυθμίσεις σύνδεσης';
+$lang['Deletion forbidden'] = 'Διαγραφή δεν επιτρέπεται';
+$lang['Delete extension'] = 'Διαγραφή επέκτασης';
+$lang['Delete link'] = 'Διαγραφή συνδέσμου';
+$lang['Delete revision'] = 'Διαγραφή αναθεώρησης';
+$lang['Delete screenshot'] = 'Διαγραφή στιγμιότυπου οθόνης';
+$lang['Description'] = 'Περιγραφή';
+$lang['Disclaimer'] = 'Αποποίηση ευθύνης';
+$lang['Disconnect'] = 'Αποσύνδεση';
+$lang['Download revision'] = 'Κατεβάστε την αναθεώρηση';
+$lang['Downloads'] = 'Λήψεις';
+$lang['Error'] = 'Σφάλμα';
+$lang['expand/collapse all'] = 'ανάπτυξη / σύμπτυξη όλων';
+$lang['extensions'] = 'επεκτάσεις';
+$lang['Extension informations'] = 'Πληροφορίες Επέκτασης ';
+$lang['Extension successfuly added. Thank you.'] = 'Η Επέκταση προστέθηκε με επιτυχία. Σας ευχαριστώ.';
+$lang['Extension successfuly deleted.'] = 'Η Επέκταση διαγράφηκε με επιτυχία.';
+$lang['File'] = 'Αρχείο';
+$lang['File too big. Filesize must not exceed %s.'] = 'Πολύ μεγάλο αρχείο. Μέγεθος δεν πρέπει να υπερβαίνει τα %s.';
+$lang['Filter'] = 'Φίλτρο';
+$lang['first'] = 'Πρώτο';
+$lang['First revision date'] = 'Πρώτη ημερομηνία αναθεώρησης';
+$lang['Hello %s'] = 'Γεια σας %s';
+$lang['Home'] = 'Αρχική';
+$lang['Identification successful'] = 'Αναγνώριση επιτυχής';
+$lang['Incorrect extension identifier'] = 'Εντοπισμός λανθασμένης επέκτασης';
+$lang['Incorrect revision identifier'] = 'Εντοπισμός λανθασμένης αναθεώρησης';
+$lang['Incorrect URL'] = 'Εσφαλμένη διεύθυνση URL';
+$lang['Incorrect username'] = 'Εσφαλμένο όνομα χρήστη';
+$lang['Incorrect username/password'] = 'Εσφαλμένο όνομα χρήστη / κωδικού πρόσβασης';
+$lang['Index'] = 'Ευρετήριο';
+$lang['last'] = 'Τελευταίο';
+$lang['Latest revision date'] = 'Τελευταία ημερομηνία αναθεώρησης';
+$lang['Link name must not be empty'] = 'Το Όνομα του συνδέσμου δεν πρέπει να είναι κενό';
+$lang['Login'] = 'Σύνδεση';
+$lang['Logout successful'] = 'Αποσύνδεση επιτυχής';
+$lang['Mail address'] = 'Ταχυδρομική διεύθυνση';
+$lang['Manage links'] = 'Διαχειριστείτε συνδέσμους';
+$lang['Manage screenshots'] = 'Διαχειριστείτε στιγμιότυπα οθόνης';
+$lang['Modify extension'] = 'Τροποποίηση επέκτασης';
+$lang['Modify revision'] = 'Τροποποίηση αναθεώρησης';
+$lang['Most recent extensions'] = 'Οι πιο πρόσφατες επεκτάσεις';
+$lang['My extensions'] = 'Οι Επεκτάσεις μου';
+$lang['My personnal homepage'] = 'Εξατομικευμένη ιστοσελίδα μου';
+$lang['Name'] = 'Όνομα';
+$lang['news feed'] = 'Ειδήσεις';
+$lang['next'] = 'Επόμενο';
+$lang['No account?'] = 'Δεν έχετε λογαριασμό?';
+$lang['No extension match your filter'] = 'Καμία επέκταση δεν ταιριάζει το φίλτρο σας ';
+$lang['No revision available for this extension.'] = 'Καμία αναθεώρηση δεν διατίθεται γι αυτή την επέκταση. Είτε γιατί δεν υπάρχει καμία απολύτως αναθεώρηση ή επειδή δεν υπάρχει καμία αναθεώρηση συμβατή με την έκδοση του φίλτρου που έχετε ορίσει.';
+$lang['no revision yet'] = 'δεν έχει ακόμη αναθεώρηση';
+$lang['Notes'] = 'Σημειώσεις';
+$lang['Only *.zip files are allowed'] = 'Επιτρέπονται μόνο *.Zip αρχεία';
+$lang['page generated in %s'] = 'Δημιουργία σελίδας σε %s';
+$lang['Password'] = 'Κωδικός';
+$lang['Personnal informations'] = 'Προσωπική πληροφορίες';
+$lang['powered by'] = 'powered by <a href="https://gna.org/projects/pem/">PEM</a>';
+$lang['prev'] = 'Προηγούμενο';
+$lang['Problem during upload'] = 'Πρόβλημα κατά την αποστολή';
+$lang['Rank'] = 'Κατάταξη';
+$lang['Register'] = 'Εγγραφή';
+$lang['Related links'] = 'Σχετικοί σύνδεσμοι';
+$lang['Released on'] = 'Κυκλοφόρησε στις';
+$lang['Reset'] = 'Επαναφορά';
+$lang['Revision'] = 'Αναθεώρηση';
+$lang['Revision list'] = 'Λίστα αναθεώρησης';
+$lang['Revision successfuly added. Thank you.'] = 'Αναθεώρηση έχει προστεθεί με επιτυχία. Σας ευχαριστώ.';
+$lang['Revision successfuly deleted.'] = 'Αναθεώρηση διαγράφηκε με επιτυχία.';
+$lang['Search'] = 'Αναζήτηση';
+$lang['See extension'] = 'Δείτε την επέκταση';
+$lang['Some fields are missing'] = 'Ορισμένοι από τα πεδία λείπουν';
+$lang['standard view'] = 'κανονική προβολή';
+$lang['Submit'] = 'Υποβολή';
+$lang['Success'] = 'Επιτυχία';
+$lang['undefined extension identifier'] = 'Απροσδιόριστο αναγνωριστικό επέκτασης';
+$lang['undefined revision identifier'] = 'Απροσδιόριστο αναγνωριστικό αναθεώρησης';
+$lang['Username already in use'] = 'Το όνομα χρήστη χρησιμοποιείται ήδη';
+$lang['Unknown extension'] = 'Άγνωστη επέκταση';
+$lang['URL'] = 'URL';
+$lang['Username'] = 'Όνομα Χρήστη';
+$lang['Version'] = 'Έκδοση';
+$lang['You can only upload PNG and JPEG files as screenshot.'] = 'Μπορείτε να φορτώσετε μόνο αρχεία JPEG και PNG ως στιγμιότυπο οθόνης.';
+$lang['You did not upload anything!'] = 'Δεν ανεβάσατε τίποτα!';
+$lang['You must be connected to add, modify or delete an extension.'] = 'Πρέπει να είστε συνδεδεμένοι για να προσθέσετε, να τροποποιήσετε ή να διαγράψετε μια επέκταση.';
+$lang['You must be connected to reach this page.'] = 'Πρέπει να είστε συνδεδεμένοι για πρόσβαση σε αυτή τη σελίδα.';
+$lang['You must be the extension author to modify it.'] = 'Πρέπει να είστε ο δημιουργός της επέκταση για να την τροποποιήσετε.';
+$lang['help'] = 'Βοήθεια';
+$lang['You must be connected to read help user'] = 'Πρέπει να είστε συνδεδεμένοι για να διαβάσετε τη βοήθεια των χρηστών';
+$lang['You mustn\'t be connected to read help guest'] = 'δεν χρειάζετε να συνδεθείτε για να διαβάσετε τη βοήθεια των επισκεπτών';
+
+$lang['Language'] = 'Γλώσσα';
+$lang['Authors'] = 'Συντάκτες';
+$lang['Manage authors'] = 'Διαχειριστείτε Δημιουργούς';
+$lang['Add an author'] = 'Προσθήκη ενός Δημιουργού';
+$lang['Are you sure you want to remove selected authors?'] = 'Είστε βέβαιοι ότι θέλετε να αφαιρέσετε τους επιλεγμένους δημιουργούς;';
+$lang['Delete selected users'] = 'Διαγραφή των επιλεγμένων χρηστών';
+$lang['You must select at least one author.'] = 'Πρέπει να επιλέξετε τουλάχιστον έναν δημιουργό.';
+$lang['This user does not exist in database.'] = 'Αυτός ο χρήστης δεν υπάρχει στη βάση δεδομένων.';
+$lang['Added by']  = 'Προστέθηκε από τον';
+$lang['Other extensions'] = 'Άλλες επεκτάσεις';
+
+$lang['SVN configuration'] = 'SVN διαμόρφωση';
+$lang['Repository URL'] = 'URL Αποθετήριου';
+$lang['Archive root directory'] = 'Φάκελλος ριζικού Αρχείου';
+$lang['Archive name'] = 'Όνομα Αρχείου';
+$lang['% will be replaced by version number'] = '% Θα αντικατασταθεί από τον αριθμό έκδοσης';
+$lang['Delete SVN data'] = 'Διαγραφή δεδομένων SVN';
+$lang['Are you sure you want to remove SVN data?'] = 'Είστε βέβαιοι ότι θέλετε να αφαιρέσετε SVN δεδομένα?';
+$lang['SVN informations'] = 'πληροφορίες SVN';
+$lang['Unable to retrieve SVN data!'] = 'Δεν είναι δυνατή η ανάκτηση δεδομένων SVN!';
+$lang['Characters not allowed in archive root directory.'] = 'Χαρακτήρες που δεν επιτρέπεται σε Φάκελλος ριζικού Αρχείου.';
+$lang['Characters not allowed in archive name.'] = 'Χαρακτήρες που δεν επιτρέπεται στο όνομα αρχείου.';
+$lang['Upload a file'] = 'Ανεβάστε ένα αρχείο';
+$lang['Use SVN file creation'] = 'Χρησιμοποιήστε δημιουργία αρχείων SVN';
+$lang['SVN revision'] = 'SVN αναθεώρηση';
+$lang['An error occured during SVN export.'] = 'Ένα σφάλμα συνέβη κατά την εξαγωγή SVN.';
+
+$lang['Available languages'] = 'Διαθέσιμες γλώσσες';
+$lang['Default description'] = 'Προεπιλεγμένη περιγραφή';
+$lang['Default description can not be empty'] = 'Προεπιλεγμένη περιγραφή δεν μπορεί να είναι κενή';
+$lang['extension'] = 'επέκταση';
+$lang['no extension'] = 'χωρίς επέκταση';
+
+$lang['Download last revision'] = 'Κατεβάστε τελευταία αναθεώρηση';
+$lang['Download'] = 'Λήψη';
+$lang['releases'] = 'εκδόσεις';
+
+$lang['Choose compatibility versions...'] = 'Επιλέξτε εκδόσεις συμβατότητας...';
+
+$lang['Tags'] = 'Ετικέτες';
+$lang['Type in a search term'] = 'Πληκτρολογήστε έναν όρο αναζήτησης';
+$lang['No results'] = 'Δεν υπάρχουν αποτελέσματα';
+$lang['Searching...'] = 'Αναζήτηση...';
+$lang['new'] = 'new';
+$lang['Rated %d times'] = 'Αξιολογήθηκε %d φορές';
+$lang['Average rating'] = 'Μεσος όρος βαθμολόγησης';
+$lang['not rated yet'] = 'Δεν έχει βαθμολογηθεί ακόμα';
+$lang['cancel this rating!'] = 'ακύρωση αυτής της βαθμολογίας!';
+$lang['Update your rating'] = 'Ενημέρωση βαθμολογία σας';
+$lang['Rate it!'] = 'Βαθμολογίστε το!';
+$lang['Your rating'] = 'Η βαθμολογία σας';
+$lang['All'] = 'Όλα';
+
+$lang['Thank you!'] = 'Ευχαριστούμαι!';
+$lang['Thank you! Your review is awaiting moderation.'] = 'Σας ευχαριστούμε! Η κριτική σας περιμένει έγκριση!';
+$lang['Please enter your name'] = 'Παρακαλώ εισάγετε το όνομα σας';
+$lang['Please enter your email'] = 'Παρακαλώ εισάγετε το email σας';
+$lang['The review is empty!'] = 'Η κριτική σας είναι κενή!';
+$lang['Please rate this extension'] = 'Παρακαλώ αξιολογήστε αυτή την επέκταση';
+$lang['Reviews'] = 'Αξιολογίσεις';
+$lang['Add a review'] = 'Προσθέστε μια αξιολόγηση';
+$lang['Email (not displayed)'] = 'Email (δεν εμφανίζεται)';
+$lang['Review summary'] = 'Συνοπτική αξιολόγηση';
+$lang['Your review'] = 'Η αξιολόγηση σας';
+$lang['Send'] = 'Αποστολή';
+$lang['Please do not use this form to request assistance or report a bug. Use the forums instead.'] = 'Παρακαλώ μην χρησιμοποιείτε αυτή τη φόρμα για να ζητήσετε βοήθεια ή να αναφέρετε ένα σφάλμα. Χρησιμοποιήστε τα φόρουμ.';
+
+?>
